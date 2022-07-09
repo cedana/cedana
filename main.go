f56031191bb6fb1818a8d0390c56a2d9a86a3eb8
nravic <niranjan96dxb@gmail.com>
2022-07-09 10:27:26 -0400

    fix: fleshing out

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package cmd
+package main
 
 import (
 	"context"
@@ -7,16 +7,39 @@ import (
 
 	pb "github.com/nravic/oort/rpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func initializeClient(client pb.OortClient) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	params, err := client.RegisterClient(ctx)
+
+	state := getState()
+	params, err := client.RegisterClient(ctx, state)
+	if err != nil {
+		log.Fatalf("client.RegisterClient failed: %v", err)
+	}
+
+	log.Println(params)
 }
 
 func runRecordState(client pb.OortClient) {
-	// get state and send to server on stream
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stream, err := client.RecordState(ctx)
+	if err != nil {
+		log.Fatalf("client.RecordState failed: %v", err)
+	}
+	for i := 1; i < 10; i++ {
+		stream.Send(&pb.ClientState{
+			Timestamp: time.Now().Unix(),
+		})
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("client.RecordState failed: %v", err)
+	}
+	log.Printf("Response: %v", reply)
 }
 
 func getState() *pb.ClientState {
@@ -28,6 +51,7 @@ func getState() *pb.ClientState {
 
 func main() {
 	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err := grpc.Dial("localhost:5000", opts...)
 	if err != nil {
 		log.Fatalf("Could not connect to RPC server %v", err)
@@ -35,5 +59,8 @@ func main() {
 
 	defer conn.Close()
 	client := pb.NewOortClient(conn)
+
+	initializeClient(client)
+	runRecordState(client)
 
 }
