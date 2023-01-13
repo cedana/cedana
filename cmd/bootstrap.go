8ecd814f69351ced60612a3eb43ff12e03aee286
Niranjan Ravichandra <nravichandra@6river.com>
2023-01-13 10:03:52 -0500

    fix: bootstrap cmd added

---
+++ cmd/bootstrap.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/nravic/cedana/utils"
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+)
+
+type Bootstrap struct {
+	l *zerolog.Logger
+}
+
+// bootstrap the cedana client and load config overrides if they exist
+var bootstrapCmd = &cobra.Command{
+	Use:   "bootstrap",
+	Short: "Setup host for cedana usage",
+	Long:  "",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logger := utils.GetLogger()
+		b := &Bootstrap{
+			l: &logger,
+		}
+		b.bootstrap()
+
+		return nil
+	},
+}
+
+func (b *Bootstrap) bootstrap() {
+	// check that ./cedana folder exists
+	homeDir := os.Getenv("HOME")
+	configFolderPath := filepath.Join(homeDir, ".cedana")
+	// check that $HOME/.cedana folder exists - create if it doesn't
+	_, err := os.Stat(configFolderPath)
+	if err != nil {
+		b.l.Info().Msg("config folder doesn't exist, creating...")
+		err = os.Mkdir(configFolderPath, 0o755)
+		if err != nil {
+			b.l.Fatal().Err(err).Msg("could not create config folder")
+		}
+	}
+
+	// check configFolderPath for cfg
+	_, err = os.OpenFile(filepath.Join(homeDir, ".cedana/client_config.json"), 0, 0o644)
+	if errors.Is(err, os.ErrNotExist) {
+		b.l.Info().Msg("client_config.json does not exist, creating sample config...")
+		// TODO: promptui?
+		// Generate some sane defaults
+		utils.GenSampleConfig(filepath.Join(configFolderPath, "client_config.json"))
+	}
+
+	// let InitConfig populate with overrides (if any)
+	_, err = utils.InitConfig()
+	if err != nil {
+		b.l.Fatal().Err(err).Msg("could not initiate generatedconfig")
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(bootstrapCmd)
+}
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"github.com/nravic/cedana/cmd"
-	"github.com/nravic/cedana/utils"
 )
 
 func main() {
-	utils.InitConfig()
 	cmd.Execute()
 }
