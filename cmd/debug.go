e7f1a6abe9eee38c6e51fdedfda3c1d9b1527ab8
Niranjan Ravichandra <nravichandra@6river.com>
2023-05-19 10:17:35 -0400

    fix: quick debugging change

---
+++ cmd/debug.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nravic/cedana/utils"
+	"github.com/spf13/cobra"
+)
+
+var debugCmd = &cobra.Command{
+	Use:    "debug",
+	Short:  "Functions/tools for debugging instances or testing new components",
+	Hidden: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return fmt.Errorf("run debug with one of its subcommands")
+	},
+}
+
+var cfgCmd = &cobra.Command{
+	Use: "config",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := utils.InitConfig()
+		if err != nil {
+			return err
+		}
+		// pretty print config for debugging to make sure it's been loaded correctly
+		prettyCfg, err := json.MarshalIndent(cfg, "", "  ")
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(prettyCfg))
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(debugCmd)
+	debugCmd.AddCommand(cfgCmd)
+}
