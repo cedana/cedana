ae93db2344cd01538830ba573ca7dd4bd1b524f4
Niranjan Ravichandra <nravic2@gmail.com>
2023-06-08 10:29:09 -0400

    fix: switch from hamming distance to levenshtein

--- utils/processes.go
+++ utils/processes.go
@@ -16,19 +16,34 @@ func GetPid(process_name string) (int32, error) {
 	similarity := 0.0
 	var proc *ps.Process
 
+	/**
+
+	The best metric here is the LEvenshtein distance (also called the edit distance). It measures the min # of
+	single character edits (insertions, deletions, subs) required to transform one into another.
+
+	It's particularily better than the other methods strutil offers w.r.t process discovery because it takes
+	command variation into account, any flexibility and I think is also faster.
+
+	**/
+	lv := metrics.NewLevenshtein()
+
 	for _, p := range processList {
-		exec, err := p.Exe()
+		exec, err := p.Cmdline()
 		if err != nil {
 			continue
 		}
 
 		// compute similarity score
-		sim := strutil.Similarity(exec, process_name, metrics.NewHamming())
-		fmt.Printf("%s:\t%f\n", exec, sim)
+		sim := strutil.Similarity(exec, process_name, lv)
 		if sim > similarity {
 			similarity = sim
 			proc = p
 		}
+	}
+
+	if proc != nil {
+		exec, _ := proc.Cmdline()
+		fmt.Printf("found process PID %d and exe %s associated with name %s\n", proc.Pid, exec, process_name)
 		return proc.Pid, nil
 	}
 
