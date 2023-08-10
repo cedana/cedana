20e66174ae9f97e7e892780199233a20f3986bc1
brandonsmith738 <boombyeyt@gmail.com>
2023-08-10 17:50:18 -0500

    fix: better formatted sh script

--- test/run_benchmarks.sh
+++ test/run_benchmarks.sh
@@ -1,24 +1,27 @@
-# Set the path to your executable
-EXECUTABLE="benchmarking/processes/loop"
-DIRECTORY="benchmarking/pids"
+#!/bin/bash
+
+# Set the path to your executable and directory
+executable="benchmarking/processes/loop"
+directory="benchmarking/pids"
 
 # Check if the executable exists
-if [ -f "$EXECUTABLE" ]; then
-
-    if [ -d "$DIRECTORY" ]; then
-        # Remove all files in the directory
-        rm -f "$DIRECTORY"/*
-        echo "All files in the directory have been removed."
-
-        "$EXECUTABLE" &
-
-        sudo /usr/local/go/bin/go test -cpuprofile benchmarking/results/cpu.prof.gz -memprofile benchmarking/results/memory.prof.gz -run=^$ -bench ^BenchmarkDump$ -parallel 4 github.com/nravic/cedana/cmd
-    else
-        echo "Directory not found: $DIRECTORY"
-    fi
-    # Execute the executable
-
-else
-    echo "Executable not found: $EXECUTABLE"
+if [ ! -f "$executable" ]; then
+    echo "Executable not found: $executable"
     exit 1
 fi
+
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+    echo "Directory not found: $directory"
+    exit 1
+fi
+
+# Remove all files in the directory
+rm -f "$directory"/*
+echo "All files in the directory have been removed."
+
+# Run the executable in the background
+"$executable" &
+
+# Run tests
+sudo /usr/local/go/bin/go test -cpuprofile benchmarking/results/cpu.prof.gz -memprofile benchmarking/results/memory.prof.gz -run=^$ -bench ^BenchmarkDump$ -parallel 4 github.com/nravic/cedana/cmd
