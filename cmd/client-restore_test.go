6c80c789f15afd8971baf3f747bae1fbd6d6affa
brandonsmith738 <boombyeyt@gmail.com>
2023-08-10 10:27:30 -0500

    fix: restore endpoint

--- cmd/client-restore_test.go
+++ cmd/client-restore_test.go
@@ -5,8 +5,20 @@ import (
 )
 
 func BenchmarkRestore(b *testing.B) {
+	c, err := instantiateClient()
+
+	if err != nil {
+		b.Errorf("Error in instantiateClient(): %v", err)
+	}
+
+	checkpoint := "../benchmarking/temp/"
+
+	if err != nil {
+		b.Errorf("Error in os.Stat(): %v", err)
+	}
+
 	for i := 0; i < b.N; i++ {
-		err := c.restore("../benchmarking/temp/")
+		err := c.restore(nil, &checkpoint)
 		if err != nil {
 			b.Errorf("Error in dump(): %v", err)
 		}
