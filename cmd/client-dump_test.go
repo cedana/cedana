616ecc4b6230fdaef4a9ea5b963061be95836b88
brandonsmith738 <boombyeyt@gmail.com>
2023-08-10 12:58:04 -0500

    fix: added sudo os work around for getting proper home path to current user

--- cmd/client-dump_test.go
+++ cmd/client-dump_test.go
@@ -8,11 +8,28 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 	"testing"
 
+	"github.com/glebarez/sqlite"
 	"github.com/nravic/cedana/utils"
+	"github.com/rs/xid"
 	"google.golang.org/protobuf/proto"
+	"gorm.io/gorm"
 )
+
+type DB struct {
+	orm *gorm.DB
+}
+
+type Benchmarks struct {
+	gorm.Model
+	ID                 string `gorm:"primaryKey"`
+	ProcessName        string
+	ProcessID          int32
+	TimeToCompleteInNS int64
+}
 
 func BenchmarkDump(b *testing.B) {
 	c, err := instantiateClient()
@@ -44,7 +61,7 @@ func BenchmarkDump(b *testing.B) {
 				return
 			}
 
-			// Convert bytes to int64
+			// Convert bytes to int32
 			pidInt32 := int32(binary.LittleEndian.Uint64(pidBytes[:]))
 			c.process.PID = pidInt32
 			break
@@ -129,11 +146,75 @@ func PostDumpCleanup() {
 	proto.Unmarshal(decompressedData, &profile)
 
 	c.logger.Log().Msgf("proto data duration: %+v", profile.DurationNanos)
+	// Here we need to add to db the profile data
+	// we also need to delete pid files and end kill processes
 }
 
 func TestMain(m *testing.M) {
 	// Code to run before the tests
+
 	m.Run()
 	// Code to run after the tests
 	PostDumpCleanup()
+	NewDB()
+}
+
+func NewDB() *DB {
+	c, err := instantiateClient()
+	if err != nil {
+		c.logger.Error().Msgf("Error in instantiateClient(): %v", err)
+	}
+
+	// this is the same code that's sitting in bootstrap right now
+	// safe to assume that bootstrap has been run (but also safe to assume it hasn't?)
+
+	// See if this works around the issue of the db not being created
+	// sudo env is different.
+	originalUser := os.Getenv("SUDO_USER")
+	homeDir := ""
+	if originalUser != "" {
+		user, err := user.Lookup(originalUser)
+		if err == nil {
+			homeDir = user.HomeDir
+		}
+	}
+	if homeDir == "" {
+		homeDir = os.Getenv("HOME")
+	}
+
+	configFolderPath := filepath.Join(homeDir, ".cedana")
+	// check that $HOME/.cedana folder exists - create if it doesn't
+	_, err = os.Stat(configFolderPath)
+	if err != nil {
+		c.logger.Log().Msg("config folder doesn't exist, creating...")
+		err = os.Mkdir(configFolderPath, 0o755)
+		if err != nil {
+			c.logger.Error().Msgf("could not create config folder: %v", err)
+		}
+	}
+
+	dbPath := filepath.Join(homeDir, ".cedana", "benchmarking.db")
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{
+		FullSaveAssociations: true,
+	})
+	if err != nil {
+		c.logger.Error().Msgf("failed to open database: %v", err)
+	}
+	db.AutoMigrate(&Benchmarks{})
+	return &DB{
+		orm: db,
+	}
+}
+
+func (db *DB) CreateJob(profile *utils.Profile, pid int32) *Benchmarks {
+	id := xid.New()
+	cj := Benchmarks{
+		ID:                 id.String(),
+		ProcessName:        "loop",
+		ProcessID:          pid,
+		TimeToCompleteInNS: profile.DurationNanos,
+	}
+	db.orm.Create(&cj)
+
+	return &cj
 }
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/afero v1.9.5
 	github.com/spf13/cobra v1.5.0
-	golang.org/x/sys v0.6.0
+	golang.org/x/sys v0.7.0
 	google.golang.org/protobuf v1.31.0
 )
 
@@ -24,18 +24,26 @@ require (
 	github.com/docker/distribution v2.8.2+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/glebarez/go-sqlite v1.21.2 // indirect
+	github.com/glebarez/sqlite v1.9.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/godbus/dbus/v5 v5.0.6 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/sys/mountinfo v0.5.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
@@ -51,6 +59,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rs/xid v1.5.0 // indirect
 	github.com/seccomp/libseccomp-golang v0.9.2-0.20220502022130-f33da4d89646 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -69,7 +79,12 @@ require (
 	golang.org/x/tools v0.6.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.3 // indirect
 	gotest.tools/v3 v3.5.0 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/sqlite v1.23.1 // indirect
 )
 
 require (
