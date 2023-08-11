a2e6b4b2ed72dc8b55e728f4f911cb6e36c64e2b
brandonsmith738 <boombyeyt@gmail.com>
2023-08-11 15:44:41 -0500

    fix: working bash script for multiple processes

--- test/run_benchmarks.sh
+++ test/run_benchmarks.sh
@@ -10,11 +10,6 @@ if [ ! -f "$execLoop" ]; then
     echo "execLoop not found: $execLoop"
     exit 1
 fi
-# Check if the execServer exists
-if [ ! -f "$execServer" ]; then
-    echo "execServer not found: $execServer"
-    exit 1
-fi
 
 # Check if the dirPids exists
 if [ ! -d "$dirPids" ]; then
@@ -28,10 +23,15 @@ echo "All files in the dirPids have been removed."
 
 # Run the execLoop in the background
 "$execLoop" &
-# Run the execServer in the background
-"$execServer" &
 
 # Run tests
 sudo /usr/local/go/bin/go test -cpuprofile benchmarking/results/cpu.prof.gz -memprofile benchmarking/results/memory.prof.gz -run=^$ -bench ^BenchmarkDumpLoop$ github.com/nravic/cedana/cmd
 wait
+# Check if the execServer exists
+if [ ! -f "$execServer" ]; then
+    echo "execServer not found: $execServer"
+    exit 1
+fi
+# Run the execServer in the background
+"$execServer" &
 sudo /usr/local/go/bin/go test -cpuprofile benchmarking/results/cpu.prof.gz -memprofile benchmarking/results/memory.prof.gz -run=^$ -bench ^BenchmarkDumpServer$ github.com/nravic/cedana/cmd
