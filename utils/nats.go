f7565ece4a9124ced09eb5c0e2480d1acbaad191
Niranjan Ravichandra <nravic2@gmail.com>
2023-09-13 11:43:20 -0400

    fix: forgot to pop nats

--- utils/nats.go
+++ utils/nats.go
@@ -11,38 +11,66 @@ import (
 )
 
 // helpers for testing NATS stuff
-func CreateTestConn(t *testing.T) *nats.Conn {
+func CreateTestConn() (*nats.Conn, error) {
 	url := fmt.Sprintf("nats://127.0.0.1:%d", nats.DefaultPort)
 	nc, err := nats.Connect(url)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
 
-	t.Cleanup(func() {
-		nc.Close()
-	})
-
-	return nc
+	return nc, nil
 }
 
-func CreateTestJetstream(t *testing.T) jetstream.JetStream {
-	nc := CreateTestConn(t)
+func CreateTestJetstream(nc *nats.Conn) (*jetstream.JetStream, error) {
 	js, err := jetstream.New(nc)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
-
-	return js
+	return &js, nil
 }
 
-func RunDefaultServer(t *testing.T) *server.Server {
+func RunDefaultServer() *server.Server {
 	opts := testserver.DefaultTestOptions
 	opts.Port = nats.DefaultPort
 	opts.Cluster.Name = "testing"
 
 	server := testserver.RunServer(&opts)
 
-	t.Cleanup(func() {
+	return server
+}
+
+func CreateBenchmarkConn(b *testing.B) *nats.Conn {
+	url := fmt.Sprintf("nats://127.0.0.1:%d", nats.DefaultPort)
+	nc, err := nats.Connect(url)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.Cleanup(func() {
+		nc.Close()
+	})
+
+	return nc
+}
+
+func CreateBnechmarkJetstream(b *testing.B) jetstream.JetStream {
+	nc := CreateBenchmarkConn(b)
+	js, err := jetstream.New(nc)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	return js
+}
+
+func RunDefaultServerBenchmark(b *testing.B) *server.Server {
+	opts := testserver.DefaultTestOptions
+	opts.Port = nats.DefaultPort
+	opts.Cluster.Name = "benchmarking"
+
+	server := testserver.RunServer(&opts)
+
+	b.Cleanup(func() {
 		server.Shutdown()
 	})
 
