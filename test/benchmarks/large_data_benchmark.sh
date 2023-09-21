393593bb9c7540d9c3ae192d1c00687e3cd179cd
Niranjan Ravichandra <nravic2@gmail.com>
2023-09-21 12:01:33 -0400

    fix: tests -> test

--- test/benchmarks/large_data_benchmark.sh
+++ test/benchmarks/large_data_benchmark.sh
@@ -85,7 +85,7 @@ cd "$current_dir" || exit
 # grep 'model name' /proc/cpuinfo | head -1 | cut -d ':' -f 2 | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' > benchmarking/temp/cpu.txt
 
 for ((i = 1; i <= num_iterations; i++)); do
-    ./tests/benchmarks/run_benchmarks.sh &
+    ./test/benchmarks/run_benchmarks.sh &
 
     # Store the process ID (PID) of the background process
     bg_pid=$!
