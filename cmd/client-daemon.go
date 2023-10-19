de64c8f5af2f0d1aee9066c2c31f69a67278f774
Brandon Smith <brandon.smith@cedana.ai>
2023-10-19 16:09:40 -0500

    chore: re-add the gd

--- cmd/client-daemon.go
+++ cmd/client-daemon.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"syscall"
+	"time"
 
 	"github.com/cedana/cedana/api"
 	"github.com/cedana/cedana/utils"
@@ -30,46 +31,46 @@ var startDaemonCmd = &cobra.Command{
 	Short: "Start daemon for cedana client. Must be run as root, needed for all other cedana functionality.",
 	Run: func(cmd *cobra.Command, args []string) {
 
-		// logger := utils.GetLogger()
+		logger := utils.GetLogger()
 
-		// executable, err := os.Executable()
-		// if err != nil {
-		// 	logger.Fatal().Msg("Could not find cedana executable")
-		// }
+		executable, err := os.Executable()
+		if err != nil {
+			logger.Fatal().Msg("Could not find cedana executable")
+		}
 
-		// ctx := &gd.Context{
-		// 	PidFileName: "/tmp/cedana.pid",
-		// 	PidFilePerm: 0o644,
-		// 	LogFileName: "/tmp/cedana-daemon.log",
-		// 	LogFilePerm: 0o664,
-		// 	WorkDir:     "./",
-		// 	Umask:       027,
-		// 	Args:        []string{executable, "daemon", "start"},
-		// }
+		ctx := &gd.Context{
+			PidFileName: "/tmp/cedana.pid",
+			PidFilePerm: 0o644,
+			LogFileName: "/tmp/cedana-daemon.log",
+			LogFilePerm: 0o664,
+			WorkDir:     "./",
+			Umask:       027,
+			Args:        []string{executable, "daemon", "start"},
+		}
 
-		// gd.AddCommand(gd.StringFlag(daemonSignal, "stop"), syscall.SIGTERM, termHandler)
+		gd.AddCommand(gd.StringFlag(daemonSignal, "stop"), syscall.SIGTERM, termHandler)
 
-		// d, err := ctx.Reborn()
-		// if err != nil {
-		// 	logger.Err(err).Msg("could not start daemon")
-		// }
+		d, err := ctx.Reborn()
+		if err != nil {
+			logger.Err(err).Msg("could not start daemon")
+		}
 
-		// if d != nil {
-		// 	return
-		// }
+		if d != nil {
+			return
+		}
 
-		// defer ctx.Release()
+		defer ctx.Release()
 
-		// logger.Info().Msgf("daemon started at %s", time.Now().Local())
+		logger.Info().Msgf("daemon started at %s", time.Now().Local())
 
-		startgRPCServer()
+		go startgRPCServer()
 
-		// err = gd.ServeSignals()
-		// if err != nil {
-		// 	logger.Fatal().Err(err)
-		// }
+		err = gd.ServeSignals()
+		if err != nil {
+			logger.Fatal().Err(err)
+		}
 
-		// logger.Info().Msg("daemon terminated")
+		logger.Info().Msg("daemon terminated")
 	},
 }
 
