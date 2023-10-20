15e33721053bc708d70580cfa8c1a97e331689d2
Brandon Smith <brandon.smith@cedana.ai>
2023-10-20 15:59:39 -0500

    sync with main

--- api/services/cts.go
+++ api/services/cts.go
@@ -69,76 +69,76 @@ func NewClient(addr string, ctx context.Context) *ServiceClient {
 	return client
 }
 
-func (c *ServiceClient) CheckpointTask(args *task.DumpArgs) *task.DumpResp {
+func (c *ServiceClient) CheckpointTask(args *task.DumpArgs) (*task.DumpResp, error) {
 	resp, err := c.services.taskService.Dump(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) RestoreTask(args *task.RestoreArgs) *task.RestoreResp {
+func (c *ServiceClient) RestoreTask(args *task.RestoreArgs) (*task.RestoreResp, error) {
 	resp, err := c.services.taskService.Restore(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) CheckpointContainer(args *task.ContainerDumpArgs) *task.ContainerDumpResp {
+func (c *ServiceClient) CheckpointContainer(args *task.ContainerDumpArgs) (*task.ContainerDumpResp, error) {
 	resp, err := c.services.taskService.ContainerDump(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) ContainerRestore(args *task.ContainerRestoreArgs) *task.ContainerRestoreResp {
+func (c *ServiceClient) RestoreContainer(args *task.ContainerRestoreArgs) (*task.ContainerRestoreResp, error) {
 	resp, err := c.services.taskService.ContainerRestore(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) CheckpointRunc(args *task.RuncDumpArgs) *task.RuncDumpResp {
+func (c *ServiceClient) CheckpointRunc(args *task.RuncDumpArgs) (*task.RuncDumpResp, error) {
 	resp, err := c.services.taskService.RuncDump(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) RuncRestore(args *task.RuncRestoreArgs) *task.RuncRestoreResp {
+func (c *ServiceClient) RuncRestore(args *task.RuncRestoreArgs) (*task.RuncRestoreResp, error) {
 	resp, err := c.services.taskService.RuncRestore(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) StartTask(args *task.StartTaskArgs) *task.StartTaskResp {
+func (c *ServiceClient) StartTask(args *task.StartTaskArgs) (*task.StartTaskResp, error) {
 	resp, err := c.services.taskService.StartTask(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) GpuCheckpoint(args *gpu.CheckpointRequest) *gpu.CheckpointResponse {
+func (c *ServiceClient) GpuCheckpoint(args *gpu.CheckpointRequest) (*gpu.CheckpointResponse, error) {
 	resp, err := c.services.gpuService.Checkpoint(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
-func (c *ServiceClient) GpuRestore(args *gpu.RestoreRequest) *gpu.RestoreResponse {
+func (c *ServiceClient) GpuRestore(args *gpu.RestoreRequest) (*gpu.RestoreResponse, error) {
 	resp, err := c.services.gpuService.Restore(c.ctx, args)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		return nil, err
 	}
-	return resp
+	return resp, nil
 }
 
 func (c *ServiceClient) Close() {
