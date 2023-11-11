a989fb17ca89197330a2f4192dadeff296075fd6
Niranjan Ravichandra <nravic2@gmail.com>
2023-11-11 17:22:11 -0500

    fix: only using psutil stats for now

--- test/benchmarks/benchmark.py
+++ test/benchmarks/benchmark.py
@@ -46,17 +46,6 @@ def measure_disk_usage(checkpoint_dir):
     return sum(os.path.getsize(os.path.join(dirpath, filename)) for dirpath, dirnames, filenames in os.walk(checkpoint_dir) for filename in filenames)
 
 
-def start_resource_measurement(pid):
-    process = get_process_by_pid(pid)
-    if process is None:
-        return None, None
-
-    mem_before = process.memory_info().rss  # Resident Set Size
-    cpu_before = process.cpu_percent(interval=1)
-    disk_before = psutil.disk_usage('/').used
-    return mem_before, cpu_before, disk_before
-
-
 def start_pprof(filename): 
     pprof_base_url = "http://localhost:6060"
     resp = requests.get(f"{pprof_base_url}/start-profiling?prefix={filename}")
@@ -69,68 +58,106 @@ def stop_pprof(filename):
     if resp.status_code != 200:
         print("error from profiler: {}".format(resp.text))
 
-def stop_resource_measurement(pid, mem_before, cpu_before, disk_before, started_at, completed_at, jobID, process_mem_used):
-    daemon = get_process_by_pid(pid)
-    if daemon is None:
-        return None, None
 
-    mem_after = daemon.memory_info().rss
-    cpu_after = daemon.cpu_percent(interval=1)
-    disk_after = psutil.disk_usage('/').used
+def start_recording(pid):
+    initial_data = {}
+    try:
+        p = psutil.Process(pid)
+        initial_data['cpu_times'] = p.cpu_times()
+        initial_data['memory'] = p.memory_info().rss
+        initial_data['disk_io'] = psutil.disk_io_counters()
+        initial_data['cpu_percent'] = p.cpu_percent(interval=None)
+        initial_data['time'] = psutil.cpu_times()
+    except psutil.NoSuchProcess:
+        print(f"No such process with PID {pid}")
 
-    mem_used = mem_after - mem_before
-    cpu_used = cpu_after - cpu_before
-    disk_used = disk_after - disk_before
+    return initial_data
+
+def stop_recording(pid, initial_data, jobID, completed_at, started_at, process_stats):
+    p = psutil.Process(pid)
+    # CPU times
+    current_cpu_times = p.cpu_times()
+    cpu_time_user_diff = current_cpu_times.user - initial_data['cpu_times'].user
+    cpu_time_system_diff = current_cpu_times.system - initial_data['cpu_times'].system
+    cpu_utilization = (cpu_time_user_diff + cpu_time_system_diff)
+    
+    cpu_time_total_diff = (cpu_time_user_diff + cpu_time_system_diff)
+         
+    #Calculate the total time all CPUs have spent since we started recording
+    current_time = psutil.cpu_times()
+    cpu_total_time_diff = sum(getattr(current_time, attr) - getattr(initial_data['time'], attr)
+                                  for attr in ['user', 'system', 'idle'])
+
+        # Calculate the percentage of CPU utilization
+    cpu_percent = 100 * cpu_time_total_diff / cpu_total_time_diff if cpu_total_time_diff else 0
+
+    # Memory usage in KB
+    current_memory = p.memory_info().rss
+    memory_used_kb = (current_memory - initial_data['memory']) / 1024
+        
+    # Disk I/O
+    current_disk_io = psutil.disk_io_counters()
+    read_count_diff = current_disk_io.read_count - initial_data['disk_io'].read_count
+    write_count_diff = current_disk_io.write_count - initial_data['disk_io'].write_count
+    read_bytes_diff = current_disk_io.read_bytes - initial_data['disk_io'].read_bytes
+    write_bytes_diff = current_disk_io.write_bytes - initial_data['disk_io'].write_bytes
+
+    # also get size of checkpoint
 
 
-    # open and write to a csv 
     with open("benchmark_output.csv", mode='a', newline='') as file:
         writer = csv.writer(file)
         # Write the headers if the file is new
         if file.tell() == 0:
-            writer.writerow(['Timestamp', 'Job ID', 'Process Memory', 'Memory Used', 'Disk Used', 'CPU Used (Percent)', 'Time Taken'])
+            writer.writerow([
+                'Timestamp', 
+                'Job ID', 
+                'Memory Used Target',
+                'Memory Used Daemon', 
+                'Write Count', 
+                'Read Count', 
+                'Write Bytes', 
+                'Read Bytes', 
+                'CPU Utilization (Secs)', 
+                'CPU Used (Percent)', 
+                'Time Taken'
+                ])
         
         # Write the resource usage data
         writer.writerow([
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),
             jobID,
-            process_mem_used, 
-            mem_used,
-            disk_used,
-            cpu_used,
+            process_stats['memory'],
+            memory_used_kb,
+            write_count_diff,
+            read_count_diff,
+            write_bytes_diff,
+            read_bytes_diff,
+            cpu_utilization,
+            cpu_percent,
             completed_at - started_at
         ])
 
+def analyze_pprof(filename):
+    pass 
 
-def run_checkpoint(daemonPID, jobID, process_pid, iteration, output_dir): 
+def run_checkpoint(daemonPID, jobID, iteration, output_dir, process_stats): 
     chkpt_cmd = "sudo ./cedana dump job {} -d tmp".format(jobID)
 
-    process = get_process_by_pid(process_pid)
-    process_mem_used = process.memory_info().rss 
-
-    mem, cpu, disk = start_resource_measurement(daemonPID)
-    checkpoint_started_at = time.perf_counter()
-
+    # TODO NR - fix 
+    initial_data = start_recording(daemonPID)
     cpu_profile_filename = "{}/cpu_{}_{}".format(output_dir, jobID, iteration)
-  
-    start_pprof(cpu_profile_filename) 
+    start_pprof(cpu_profile_filename)
+
+    checkpoint_started_at = time.monotonic_ns() 
     p = subprocess.Popen(["sh", "-c", chkpt_cmd], stdout=subprocess.PIPE)
     # used for capturing full time instead of directly exiting
     p.wait()
-    checkpoint_completed_at = time.perf_counter()
-    stop_pprof(cpu_profile_filename)
 
-    # also writes to a csv 
-    stop_resource_measurement(
-        daemonPID, 
-        mem, 
-        cpu, 
-        disk, 
-        checkpoint_started_at, 
-        checkpoint_completed_at, 
-        jobID, 
-        process_mem_used
-        )
+    # these values have an error range in 35ms! I blame Python?
+    checkpoint_completed_at = time.monotonic_ns()  
+    stop_recording(daemonPID, initial_data, jobID, checkpoint_completed_at, checkpoint_started_at, process_stats)
+    stop_pprof(cpu_profile_filename)
 
 def run_restore(jobID):
     restore_started_at = time.perf_counter()
@@ -144,26 +171,26 @@ def run_restore(jobID):
     print("completed restore of process at {}".format(restore_completed_at))
 
 def run_exec(cmd, jobID): 
+    process_stats = {}
     exec_cmd = "./cedana exec {} {}".format(cmd, jobID)
 
     process = subprocess.Popen(["sh", "-c", exec_cmd], stdout=subprocess.PIPE)
     pid = int(process.communicate()[0].decode().strip())
-    return pid 
+    process_stats['pid'] = pid
 
-def analyze_pprof(output_dir):
-    pass 
+    psutil_process = psutil.Process(pid)
+    process_stats['memory'] = psutil_process.memory_info().rss / 1024
+
+    return process_stats 
 
 def push_to_bigquery():
-# Initialize a BigQuery client
     client = bigquery.Client()
 
     dataset_id = 'devtest'
     table_id = 'benchmarking_naiive'
 
-# Set the path to your CSV file
     csv_file_path = 'benchmark_output.csv'
 
-# Create a job config
     job_config = LoadJobConfig(
         source_format=SourceFormat.CSV,
         skip_leading_rows=1,  # Change this according to your CSV file
@@ -171,7 +198,6 @@ def push_to_bigquery():
         write_disposition="WRITE_TRUNCATE",  # Options are WRITE_APPEND, WRITE_EMPTY, WRITE_TRUNCATE
 )
 
-# Get the dataset and table reference
     dataset_ref = client.dataset(dataset_id)
     table_ref = dataset_ref.table(table_id)
 
@@ -206,18 +232,18 @@ def main():
     ]
 
     # run in a loop 
-    num_samples = 20
+    num_samples = 30
     for x in range(len(jobIDs)): 
         jobID = jobIDs[x]
         for y in range(num_samples):
-            process_pid = run_exec(cmds[x], jobID)
+            process_stats = run_exec(cmds[x], jobID)
             time.sleep(1)
-            run_checkpoint(daemon_pid, jobID, process_pid, y, output_dir)
+            run_checkpoint(daemon_pid, jobID, y, output_dir, process_stats)
             time.sleep(1)
-
-    # todo - run analysis on pprof and make new csv 
 
     push_to_bigquery()
 
     # delete benchmarking folder
     cleanup()
+
+main()
