7e42a3442c0bafebdc9931abdd6ddff7951a294f
Niranjan Ravichandra <nravic2@gmail.com>
2023-11-30 10:42:49 -0500

    fix: mounting config from script

--- scripts/ci/benchmark-and-push.sh
+++ scripts/ci/benchmark-and-push.sh
@@ -24,18 +24,21 @@ then
     exit 1
 fi 
 
-if [ -z "$CHECKPOINT_SERVER_ADDR" ]
+if [ -z "$CHECKPOINTSVC_URL" ]
 then
-    echo "CHECKPOINT_SERVER_ADDR is not set"
+    echo "CHECKPOINTSVC_URL is not set"
     exit 1
 fi
 
 CONTAINER_CREDENTIAL_PATH=/tmp/creds.json 
 
+echo '{"client":{"leave_running":false,"signal_process_pre_dump":false,"signal_process_timeout":0,"task":""}, "connection": {"cedana-auth-token": "random-token", "cedana_url": "'$CHECKPOINTSVC_URL'", "cedana_user": "benchmark"}}' > client_config.json
 
 sudo docker run \
  -v $GOOGLE_APPLICATION_CREDENTIALS:$CONTAINER_CREDENTIAL_PATH \
+ -v client_config.json:/home/.cedana/client_config.json \
  -e GOOGLE_APPLICATION_CREDENTIALS=$CONTAINER_CREDENTIAL_PATH \
- -e PROJECT_ID=cedana-benchmarking --privileged --tmpfs /run  ghcr.io/cedana/cedana-benchmarking:latest
+ -e PROJECT_ID=cedana-benchmarking \ 
+ -e CHECKPOINTSVC_URL=$CHECKPOINTSVC_URL --privileged --tmpfs /run  ghcr.io/cedana/cedana-benchmarking:latest
 
 
