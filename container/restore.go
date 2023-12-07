fda288365d021e7ee88f84adc1e6b7fcd2a0a149
Brandon Smith <brandon.smith@cedana.ai>
2023-12-06 18:13:20 -0600

    fix: manually add another ext bind mnt

--- container/restore.go
+++ container/restore.go
@@ -122,7 +122,7 @@ func RuncRestore(imgPath string, containerId string, opts RuncOpts) error {
 	criuOpts := CriuOpts{
 		ImagesDirectory: imgPath,
 		WorkDirectory:   "",
-		External:        []string{"mnt[k8sSecrets]:/var/run/secrets/kubernetes.io/serviceaccount"},
+		External:        []string{"mnt[k8sSecrets]:/var/run/secrets/kubernetes.io/serviceaccount", "mnt[k8sHostname]:/etc/hostname"},
 	}
 
 	runcOpts := &RuncOpts{
