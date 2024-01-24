e339e3e0436598e8fc9e7f29018e0deba7eec432
Niranjan Ravichandra <nravic@cedana.ai>
2024-01-24 16:00:26 -0500

    fix: better colors and title

--- test/gpu/viz.py
+++ test/gpu/viz.py
@@ -1,5 +1,3 @@
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import traceback
@@ -25,23 +23,26 @@ def animate(i, filename, ax1, ax2):
     ax2.clear()
     
     ax1.set_xlabel('Number of Elements')
-    ax1.set_ylabel('Time (ms)', color='tab:red')
+    ax1.set_ylabel('Time (ms)', color='tab:green')
+    ax2.set_ylabel('Mega-elements/sec', color='tab:blue')
 
     if sizes:
-        ax1.plot(sizes, times, color='tab:red')
+        ax1.plot(sizes, times, color='tab:green')
         ax2.plot(sizes, rates, color='tab:blue')
 
 # Setup plot
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-plt.title('GPU Sorting Performance')
+plt.title('GPU Quicksort Performance')
 
-filename = '/var/log/cedana-output.log'  # Replace with your actual file name
+filename = 'out.txt'  # Replace with your actual file name
 
 # Initialize plot
 ax1.set_xlabel('Number of Elements')
-ax1.set_ylabel('Time (ms)', color='tab:red')
+ax1.set_ylabel('Time (ms)', color='tab:green')
+ax2.set_ylabel('Mega-elements/sec', color='tab:blue')
 
 # Creating animation
 ani = FuncAnimation(fig, animate, fargs=(filename, ax1, ax2), interval=100)
 plt.show()
+
