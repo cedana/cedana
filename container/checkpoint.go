1eb559be1fa188c5e5cb145ab52504687944bc4a
Brandon Smith <brandon.smith@cedana.ai>
2024-02-15 11:03:41 -0600

    increase timeout

--- container/checkpoint.go
+++ container/checkpoint.go
@@ -641,7 +641,7 @@ func loadState(root string) (*State, error) {
 }
 
 func newContainerdClient(ctx gocontext.Context, opts ...containerd.ClientOpt) (*containerd.Client, gocontext.Context, gocontext.CancelFunc, error) {
-	timeoutOpt := containerd.WithTimeout(0)
+	timeoutOpt := containerd.WithTimeout(300)
 	containerdEndpoint := "/run/containerd/containerd.sock"
 	if _, err := os.Stat(containerdEndpoint); err != nil {
 		containerdEndpoint = "/host/run/k3s/containerd/containerd.sock"
