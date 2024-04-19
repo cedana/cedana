b0e40a2f30f48369b670d0ea6940d71740959e2e
Yash Anand <yashanand1910@gmail.com>
2024-04-19 18:41:54 -0400

    Clean user env file

--- .env.sh
+++
@@ -1,4 +0,0 @@
-export CEDANA_GPU_ENABLED=true 
-export GPU_CONTROLLER_PATH=/home/nravic/go/src/github.com/cedana/cedana-gpu/build/gpu-controller
-export LD_PRELOAD=/home/nravic/go/src/github.com/cedana/cedana-gpu/build/libcedana-gpu.so
-export CRIU_LIBS_DIR=/home/nravic/code/criu/plugins
