371e503d4e5463fe95c877b8f03179cbd608f618
Niranjan Ravichandra <nravic2@gmail.com>
2024-05-21 14:14:02 -0400

    fix: no more docker

--- scripts/ci/smoke-test.sh
+++ scripts/ci/smoke-test.sh
@@ -7,12 +7,24 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable test"
 
-./apt-install.sh docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+APT_PACKAGES="wget git make libnl-3-dev libnet-dev \
+    libbsd-dev python-ipaddress libcap-dev \
+    libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler \
+    protobuf-compiler python3-protobuf"
+
+install_apt_packages() {
+    apt-get update
+    apt-get install -y $APT_PACKAGES
+}
 
 if [ -z "$DOCKERHUB_TOKEN" ]; then
     echo "DOCKERHUB_TOKEN is not set"
     exit 1
 fi
+
+print_header() {
+    echo "############### $1 ###############"
+}
 
 print_env() {
     set +x
@@ -44,23 +56,42 @@ print_env() {
     set -x
 }
 
+setup_ci() {
+    install_apt_packages
+    wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz && rm -rf /usr/local/go
+    tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz && rm go1.22.0.linux-amd64.tar.gz
+
+    export PATH=$PATH:/usr/local/go/bin
+    echo "export PATH=$PATH:/usr/local/go/bin" >>/root/.bashrc
+
+    # Install CRIU
+    git clone https://github.com/checkpoint-restore/criu.git && cd /criu
+    git checkout master && git pull
+    make
+    cp criu/criu /usr/local/bin/criu
+    cd /
+
+    # Install Cedana
+    git clone https://github.com/cedana/cedana && mkdir ~/.cedana
+    git fetch && git checkout ${BRANCH_NAME} && git pull origin ${BRANCH_NAME}
+
+    # Install smoke & bench deps
+    pip3 install -r test/benchmarks/requirements.txt
+}
+
+start_cedana() {
+    ./build-start-daemon.sh
+}
+
+start_smoke() {
+    sudo -E python3 test/benchmarks/performance.py --smoke --num_samples 3
+}
+
 print_env
 
-sudo docker pull cedana/cedana-benchmarking:latest
-
-echo '{"client":{"leave_running":false, "task":""}}' >client_config.json
+echo '{"client":{"leave_running":false, "task":""}}' >~/.cedana/client_config.json
 BRANCH_NAME="${CI_BRANCH:-main}"
 
-sudo docker run \
-    -e CEDANA_OTEL_ENABLED=false \
-    -v ${PWD}/client_config.json:/root/.cedana/client_config.json \
-    --cap-add=SYS_ADMIN \
-    --privileged --tmpfs /run \
-    --entrypoint /bin/bash \
-    cedana/cedana-benchmarking:latest -c "
-    git fetch origin &&
-    git checkout ${BRANCH_NAME} &&
-    git pull origin ${BRANCH_NAME} &&
-    ./build-start-daemon.sh &&
-    sudo -E python3 test/benchmarks/performance.py --smoke --num_samples 3
-"
+setup_ci
+start_cedana
+start_smoke
