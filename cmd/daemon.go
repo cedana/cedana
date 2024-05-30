85a9ffe4882cf6a8bfac3a4d8416498c52f4001b
Niranjan Ravichandra <nravic2@gmail.com>
2024-05-30 13:31:15 -0400

    fix: multiple cuda version workaround

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -106,7 +106,7 @@ func pullGPUBinary(ctx context.Context, binary string, filePath string, version 
 	logger := ctx.Value("logger").(*zerolog.Logger)
 	_, err := os.Stat(filePath)
 	if err == nil {
-		logger.Debug().Msgf("binary exists at %s, doing nothing", filePath)
+		logger.Debug().Msgf("binary exists at %s, doing nothing. Delete existing binary to download another supported cuda version.", filePath)
 		// TODO NR - check version and checksum of binary?
 		return nil
 	}
