904e704eb36323f39da14dd38e8136b1fab6c760
Brandon Smith <brandon.smith@cedana.ai>
2024-06-03 15:08:07 -0500

    fix: add pid flag runc dump

--- cmd/dump.go
+++ cmd/dump.go
@@ -273,6 +273,7 @@ func init() {
 	dumpRuncCmd.Flags().BoolP(tcpEstablishedFlag, "t", false, "tcp established")
 	dumpRuncCmd.Flags().StringP(containerRootFlag, "r", "k8s", "container root")
 	dumpRuncCmd.Flags().BoolP(gpuEnabledFlag, "g", false, "gpu enabled")
+	dumpRuncCmd.Flags().Int32P(pidFlag, "p", 0, "pid")
 
 	rootCmd.AddCommand(dumpCmd)
 }
