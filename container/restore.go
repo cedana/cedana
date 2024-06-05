af5569f9081ccb2e3b749dbade5319dca0c99265
Brandon Smith <brandon.smith@cedana.ai>
2024-06-05 17:20:51 -0500

    fix: drop previous tests of bind mounts

--- container/restore.go
+++ container/restore.go
@@ -166,99 +166,10 @@ func RuncRestore(imgPath string, containerId string, opts RuncOpts) error {
 		}
 	}
 
-	sysboxMounts := []rspec.Mount{
-		{
-			Destination: "/var/lib/kubelet",
-			Source:      filepath.Join("/var/lib/sysbox/kubelet", bundleID),
-		},
-		{
-			Destination: "/var/lib/rancher/rke2",
-			Source:      filepath.Join("/var/lib/sysbox/rancher-rke2", bundleID),
-		},
-		{
-			Destination: "/var/lib/sysbox/rancher-k3s/",
-			Source:      filepath.Join("/var/lib/sysbox/rancher-k3s", bundleID),
-		},
-		{
-			Destination: "/var/lib/rancher/k3s",
-			Source:      filepath.Join("/var/lib/sysbox/rancher-k3s", bundleID),
-		},
-		{
-			Destination: "/var/lib/docker",
-			Source:      filepath.Join("/var/lib/sysbox/docker", bundleID),
-		},
-		{
-			Destination: "/var/lib/containerd/io.containerd.snapshotter.v1.overlayfs",
-			Source:      filepath.Join("/var/lib/sysbox/containerd", bundleID),
-		},
-		{
-			Destination: "/var/lib/buildkit",
-			Source:      filepath.Join("/var/lib/sysbox/buildkit", bundleID),
-		},
-		{
-			Destination: "/var/lib/k0s",
-			Source:      filepath.Join("/var/lib/sysbox/k0s", bundleID),
-		},
-
-		{
-			Destination: "/dev/kmsg",
-			Source:      "/null",
-		},
-		{
-			Destination: "/proc/uptime",
-			Source:      "/proc/uptime",
-		},
-		{
-			Destination: "/proc/sys",
-			Source:      "/proc/sys",
-		},
-		{
-			Destination: "/proc/swaps",
-			Source:      "/proc/swaps",
-		},
-		{
-			Destination: "/sys/module/nf_conntrack/parameters",
-			Source:      "/sys/module/nf_conntrack/parameters",
-		},
-		{
-			Destination: "/sys/kernel",
-			Source:      "/sys/kernel",
-		},
-		{
-			Destination: "/sys/devices/virtual",
-			Source:      "/sys/devices/virtual",
-		},
-	}
-
-	srcFiles, err := os.ReadDir("/usr/src/")
-	if err != nil {
-		return err
-	}
-
-	for _, file := range srcFiles {
-		mount := rspec.Mount{
-			Destination: filepath.Join("/usr/src", file.Name()),
-			Source:      filepath.Join("/usr/src", file.Name()),
+	for _, m := range stateSpec.Mounts {
+		if m.Type == "bind" {
+			externalMounts = append(externalMounts, fmt.Sprintf("mnt[%s]:%s", m.Destination, m.Source))
 		}
-		sysboxMounts = append(sysboxMounts, mount)
-	}
-
-	moduleFiles, err := os.ReadDir("/usr/lib/modules/")
-	if err != nil {
-		return err
-	}
-
-	for _, file := range moduleFiles {
-		mount := rspec.Mount{
-			Destination: filepath.Join("/lib/modules", file.Name()),
-			Source:      filepath.Join("/usr/lib/modules", file.Name()),
-		}
-		sysboxMounts = append(sysboxMounts, mount)
-	}
-
-	// TODO make this sysbox only
-	for _, m := range sysboxMounts {
-		externalMounts = append(externalMounts, fmt.Sprintf("mnt[%s]:%s", m.Destination, m.Source))
 	}
 
 	criuOpts := CriuOpts{
@@ -279,10 +190,11 @@ func RuncRestore(imgPath string, containerId string, opts RuncOpts) error {
 		NetPid:        opts.NetPid,
 	}
 
-	_, err = StartContainer(runcOpts, CT_ACT_RESTORE, &criuOpts)
+	_, err := StartContainer(runcOpts, CT_ACT_RESTORE, &criuOpts)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
