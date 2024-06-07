3c2713ae497eaef06d76fccbc642df299eebcf96
Brandon Smith <brandon.smith@cedana.ai>
2024-06-07 09:46:06 -0500

    feat: dumprootfs

--- api/containerd/containerd.go
+++ api/containerd/containerd.go
@@ -1,7 +1,11 @@
 package containerd
 
 import (
+	"fmt"
+
 	"github.com/containerd/containerd"
+	"github.com/containerd/errdefs"
+	"golang.org/x/net/context"
 )
 
 type ContainerdService struct {
@@ -19,7 +23,39 @@ func New(address string) (*ContainerdService, error) {
 	return &ContainerdService{client}, nil
 }
 
-func (*ContainerdService) DumpRootfs() (string, error) {
+func (service *ContainerdService) DumpRootfs(ctx context.Context, containerID, imageRef string) (string, error) {
+	opts := []containerd.CheckpointOpts{
+		containerd.WithCheckpointRuntime,
+		containerd.WithCheckpointImage,
+		containerd.WithCheckpointRW,
+	}
 
-	return "NOT IMPLEMENTED", nil
+	container, err := service.client.LoadContainer(ctx, containerID)
+	if err != nil {
+		return "", err
+	}
+
+	task, err := container.Task(ctx, nil)
+	if err != nil {
+		if !errdefs.IsNotFound(err) {
+			return "", err
+		}
+	}
+	// pause if running
+	if task != nil {
+		if err := task.Pause(ctx); err != nil {
+			return "", err
+		}
+		defer func() {
+			if err := task.Resume(ctx); err != nil {
+				fmt.Println(fmt.Errorf("error resuming task: %w", err))
+			}
+		}()
+	}
+
+	if _, err := container.Checkpoint(ctx, imageRef, opts...); err != nil {
+		return "", err
+	}
+
+	return imageRef, nil
 }
