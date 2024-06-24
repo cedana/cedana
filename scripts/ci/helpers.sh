df85b2393c5b5728fdf7c3f67d3845d06d2c03bb
Niranjan Ravichandra <nravic@cedana.ai>
2024-06-24 09:08:52 -0400

    fix: install btrfs, libapparmor, libseccomp on ci

--- scripts/ci/helpers.sh
+++ scripts/ci/helpers.sh
@@ -2,6 +2,7 @@
 
 APT_PACKAGES="wget git make curl libnl-3-dev libnet-dev \
     libbsd-dev python-ipaddress libcap-dev \
+    btrfs-progs libseccomp-dev libapparmor-dev \
     libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler \
     protobuf-compiler python3-protobuf"
 
