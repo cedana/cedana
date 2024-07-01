338c20ab3c6b9383b9ee7a0d9a8f2851380d2dee
Brandon Smith <brandon.smith@cedana.ai>
2024-07-01 18:05:21 -0500

    fix: missing buildah dep for crio rootfs c/r

--- scripts/k8s/setup-host.sh
+++ scripts/k8s/setup-host.sh
@@ -6,13 +6,13 @@ chroot /host <<"EOT"
 YUM_PACKAGES="wget git gcc make libnet-devel protobuf \
     protobuf-c protobuf-c-devel protobuf-c-compiler \
     protobuf-compiler protobuf-devel python3-protobuf \
-    libnl3-devel libcap-devel libseccomp-devel gpgme-devel btrfs-progs-devel"
+    libnl3-devel libcap-devel libseccomp-devel gpgme-devel btrfs-progs-devel buildah"
 
 APT_PACKAGES="wget git make libnl-3-dev libnet-dev \
     libbsd-dev libcap-dev pkg-config \
     libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler pkg-config \
     protobuf-compiler python3-protobuf build-essential \
-    libgpgme-dev libseccomp-dev libbtrfs-dev"
+    libgpgme-dev libseccomp-dev libbtrfs-dev buildah"
 
 install_apt_packages() {
     apt-get update
