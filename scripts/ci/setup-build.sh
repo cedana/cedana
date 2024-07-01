293c817a3896cda88b630f0942f389288dd3dab0
Niranjan Ravichandra <nravic@cedana.ai>
2024-07-01 19:34:59 -0400

    fix: missing dot

--- scripts/ci/setup-build.sh
+++ scripts/ci/setup-build.sh
@@ -3,7 +3,7 @@
 source ./helpers.sh
 
 main() {
-    pushd ../. && echo "Running setup_ci_build in $(pwd)" &&
+    pushd ../.. && echo "Running setup_ci_build in $(pwd)" &&
     setup_ci_build || { echo "setup_ci_build failed"; exit 1; }
     popd
 }
