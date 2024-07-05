c23f41b3a51470bb3b2437552a548981aa5dfa8b
Niranjan Ravichandra <nravic2@gmail.com>
2024-07-05 11:42:51 -0400

    fix: source in smoke too

--- scripts/ci/smoke-test.sh
+++ scripts/ci/smoke-test.sh
@@ -15,6 +15,7 @@ main() {
     pushd ../.. && echo "Starting smoke test in cwd: $(pwd)"
     print_env || { echo "print_env failed"; exit 1; }
     setup_ci || { echo "setup_ci failed"; exit 1; }
+    source_env || { echo "source_env failed"; exit 1; }
     start_cedana || { echo "start_cedana failed"; exit 1; }
     start_smoke || { echo "start_smoke failed"; exit 1; }
     stop_cedana || { echo "stop_cedana failed"; exit 1; }
