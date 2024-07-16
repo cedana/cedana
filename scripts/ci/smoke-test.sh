ff12eca957504346f8f98d0b0f7d3b9e52331f8f
Yash Anand <yashanand1910@gmail.com>
2024-07-16 15:15:26 -0400

    Add failure to Makefile

--- scripts/ci/smoke-test.sh
+++ scripts/ci/smoke-test.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 # Used to run a quick smoke test for CI
 
 source ./helpers.sh
 
 start_smoke() {
+    echo "Running smoke test in cwd: $(pwd)"
     sudo -E python3 test/benchmarks/performance.py --smoke --num_samples 1
     if [[ $? -ne 0 ]]; then
         echo "start_smoke failed!"
@@ -11,14 +12,21 @@ start_smoke() {
     fi
 }
 
+cleanup() {
+    echo "Cleaning up..."
+    pushd ../..
+    stop_cedana
+    popd
+}
+trap cleanup EXIT
+
 main() {
-    pushd ../.. && echo "Starting smoke test in cwd: $(pwd)"
-    print_env || { echo "print_env failed"; exit 1; }
-    setup_ci || { echo "setup_ci failed"; exit 1; }
-    source_env || { echo "source_env failed"; exit 1; }
-    start_cedana || { echo "start_cedana failed"; exit 1; }
-    start_smoke || { echo "start_smoke failed"; exit 1; }
-    stop_cedana || { echo "stop_cedana failed"; exit 1; }
+    pushd ../..
+    print_env
+    setup_ci
+    source_env
+    start_cedana
+    start_smoke
     popd
 }
 
