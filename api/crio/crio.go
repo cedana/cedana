87d0a1bdd3c129de19c1e6fa420bf96b9be1b9c1
Brandon Smith <brandon.smith@cedana.ai>
2024-07-18 18:58:38 -0500

    fix: decode auth data and split at : to get username and password

--- api/crio/crio.go
+++ api/crio/crio.go
@@ -415,7 +415,13 @@ func ImagePush(ctx context.Context, newImageRef string) error {
 
 		decodedAuthString := string(decodedAuthBytes)
 
+		logger.Debug().Msgf("decoded string: %s", decodedAuthString)
+
 		parts := strings.Split(decodedAuthString, ":")
+
+		if len(parts) != 2 {
+			return fmt.Errorf("decoded auth string is not correctly formatted")
+		}
 
 		loginOpts.Username = parts[0]
 		loginOpts.Password = parts[1]
