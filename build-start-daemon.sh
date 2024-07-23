57ac98b0bc59cabd2e27cb71fec94319854efb07
Swarnim Arun <swarnimarun11@gmail.com>
2024-07-23 16:01:57 +0530

    fix: logs and remove redundant cp

--- build-start-daemon.sh
+++ build-start-daemon.sh
@@ -56,11 +56,11 @@ if [ $NO_BUILD -ne 1 ]; then
         echo "Build failed. Exiting."
         exit 1
     else
+        echo "Build successful. Copying the built \`cedana\` binary"
         $SUDO_USE cp $APP_NAME $APP_PATH
     fi
 else
     echo "Skipping build..."
-    $SUDO_USE cp $APP_NAME $APP_PATH
 fi
 
 if [ "$CEDANA_GPU_ENABLED" = "true" ]; then
