32208f7dedbf5ed1110952f3fcfcb12b5206f83f
Yash Anand <yashanand1910@gmail.com>
2024-08-02 17:48:34 -0400

    Fix args passing in start_cedana

--- test/regression/helper.bash
+++ test/regression/helper.bash
@@ -3,7 +3,7 @@
 # Helper functions that hit the local Cedana API
 
 function start_cedana() {
-    ./build-start-daemon.sh --no-build --args=$@
+    ./build-start-daemon.sh --no-build --args="$@"
 }
 
 function stop_cedana() {
@@ -27,46 +27,46 @@ function restore_task() {
 }
 
 function start_busybox(){
-  local container_name="$1"
+    local container_name="$1"
 
-  sudo ctr image pull docker.io/library/busybox:latest
+    sudo ctr image pull docker.io/library/busybox:latest
 
-  sudo ctr run -d docker.io/library/busybox:latest "$container_name" sh -c 'while true; do sleep 3600; done'
+    sudo ctr run -d docker.io/library/busybox:latest "$container_name" sh -c 'while true; do sleep 3600; done'
 }
 
 function rootfs_checkpoint() {
-  local container_id="$1"
-  local image_ref="$2"
-  local containerd_sock="$3"
-  local namespace="$4"
+    local container_id="$1"
+    local image_ref="$2"
+    local containerd_sock="$3"
+    local namespace="$4"
 
-  cedana dump rootfs -p "$container_id" --ref "$image_ref" -a "$containerd_sock" -n "$namespace"
+    cedana dump rootfs -p "$container_id" --ref "$image_ref" -a "$containerd_sock" -n "$namespace"
 }
 
 function rootfs_restore() {
-  local container_id="$1"
-  local image_ref="$2"
-  local containerd_sock="$3"
-  local namespace="$4"
+    local container_id="$1"
+    local image_ref="$2"
+    local containerd_sock="$3"
+    local namespace="$4"
 
-  cedana restore rootfs -p "$container_id" --ref "$image_ref" -a "$containerd_sock" -n "$namespace"
+    cedana restore rootfs -p "$container_id" --ref "$image_ref" -a "$containerd_sock" -n "$namespace"
 }
 
 function runc_checkpoint() {
-  local dir="$1"
-  local job_id="$2"
-  cedana dump runc --dir "$dir" --id "$job_id"
+    local dir="$1"
+    local job_id="$2"
+    cedana dump runc --dir "$dir" --id "$job_id"
 }
 
 function runc_restore() {
-  local bundle="$1"
-  local dir="$2"
-  local id="$3"
-  local tty="$4"
-  cedana restore runc -e -b "$bundle" --dir "$dir" --id "$id" --console-socket "$tty"
+    local bundle="$1"
+    local dir="$2"
+    local id="$3"
+    local tty="$4"
+    cedana restore runc -e -b "$bundle" --dir "$dir" --id "$id" --console-socket "$tty"
 }
 
 function fail() {
-  echo "$@" >&2
-  exit 1
+    echo "$@" >&2
+    exit 1
 }
