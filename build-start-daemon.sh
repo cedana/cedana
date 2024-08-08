8de85bf728c3dcdadeaaf528d946ce60c4336679
Niranjan Ravichandra <nravic@cedana.ai>
2024-08-08 13:55:07 -0400

    fix: suppress one warning

--- build-start-daemon.sh
+++ build-start-daemon.sh
@@ -56,6 +56,7 @@ if [ $NO_BUILD -ne 1 ]; then
 
     CGO_ENABLED=1 go build -ldflags "$LDFLAGS"
 
+    # shellcheck disable=code # I like this so not changing
     if [ $? -ne 0 ]; then
         echo "Build failed. Exiting."
         exit 1
