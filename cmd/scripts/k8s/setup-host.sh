e743112b7559ee0709e14a428007621094eee85e
Swarnim Arun <swarnimarun11@gmail.com>
2024-08-21 22:13:34 +0530

    fix: revert setup changes to working base
    only tested debian fedora might still be broken

--- cmd/scripts/k8s/setup-host.sh
+++ cmd/scripts/k8s/setup-host.sh
@@ -6,76 +6,17 @@ chroot /host /bin/bash -c '
 # Ensure non-interactive mode for package managers
 export DEBIAN_FRONTEND=noninteractive
 
-if [[ $SKIPSETUP -eq 1 ]]; then
-    cd /
-    IS_K8S=1 ./build-start-daemon.sh --systemctl --no-build
-    exit 0
-fi
-
 YUM_PACKAGES=(wget libnet-devel libnl3-devel libcap-devel libseccomp-devel gpgme-devel btrfs-progs-devel buildah criu protobuf protobuf-c protobuf-c-devel protobuf-c-compiler protobuf-compiler protobuf-devel python3-protobuf)
-APT_PACKAGES=(wget libnl-3-dev libnet-dev libbsd-dev libcap-dev pkg-config libgpgme-dev libseccomp-dev libbtrfs-dev buildah libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python3-protobuf)
 
-check_and_install_apt_packages() {
-    apt-get update -y
+APT_PACKAGES=(wget git make libnl-3-dev libnet-dev libbsd-dev libcap-dev pkg-config libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler pkg-config protobuf-compiler python3-protobuf build-essential libgpgme-dev libseccomp-dev libbtrfs-dev buildah)
 
-    local missing_packages=()
-    for pkg in "${APT_PACKAGES[@]}"; do
-        if ! dpkg -l | grep -qw "$pkg"; then
-            missing_packages+=("$pkg")
-        fi
-    done
-
-    # Check if CRIU is installed
-    if ! dpkg -l | grep -qw "criu"; then
-        case $(uname -m) in
-        x86_64)
-            PACKAGE_URL="https://download.opensuse.org/repositories/devel:/tools:/criu/xUbuntu_22.04/amd64/criu_3.19-4_amd64.deb"
-            OUTPUT_FILE="criu_3.19-4_amd64.deb"
-        aarch64)
-            PACKAGE_URL="https://download.opensuse.org/repositories/devel:/tools:/criu/xUbuntu_22.04/arm64/criu_3.19-4_arm64.deb"
-            OUTPUT_FILE="criu_3.19-4_arm64.deb"
-        *)
-            echo "Unknown platform $(uname -m)"
-            exit 1
-        esac
-
-        wget "$PACKAGE_URL" -O "$OUTPUT_FILE"
-        dpkg -i "$OUTPUT_FILE"
-    else
-        echo "CRIU is already installed"
-    fi
-
-    # Install other APT packages if missing
-    if [ ${#missing_packages[@]} -gt 0 ]; then
-        apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "${missing_packages[@]}"
-    else
-        echo "All APT packages are already installed"
-    fi
-
+install_apt_packages() {
+    apt-get update
+    apt-get install -y "${APT_PACKAGES[@]}" || echo "Failed to install and setup packages"
 }
 
 check_and_install_yum_packages() {
-    local missing_packages=()
-    for pkg in "${YUM_PACKAGES[@]}"; do
-        if ! rpm -q "$pkg" &>/dev/null; then
-            missing_packages+=("$pkg")
-        fi
-    done
-
-    # Check if CRIU is installed
-    if ! rpm -q "criu" &>/dev/null; then
-        yum install -y criu
-    else
-        echo "CRIU is already installed"
-    fi
-
-    # Install other YUM packages if missing
-    if [ ${#missing_packages[@]} -gt 0 ]; then
-        yum install -y "${missing_packages[@]}"
-        yum group install -y "Development Tools"
-    else
-        echo "All YUM packages are already installed"
-    fi
+    yum install -y "${YUM_PACKAGES[@]}" || echo "Failed to install and setup packages"
 }
 
 install_criu_ubuntu_2204() {
