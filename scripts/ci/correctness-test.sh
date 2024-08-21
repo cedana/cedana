7427b35f5de6395ef2aedd8fa1004d872edbb3b3
Yash Anand <yashanand1910@gmail.com>
2024-08-21 14:43:16 -0400

    Move correctness tests as a separate CI job

---
+++ scripts/ci/correctness-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+# Used to run correctness tests for CI
+
+source ./helpers.sh
+
+start_correctness() {
+    echo "Running correctness test in cwd: $(pwd)"
+    sudo -E python3 test/benchmarks/performance.py --correctness --verbose
+    if [[ $? -ne 0 ]]; then
+        echo "start_correctness failed!"
+        exit 1
+    fi
+}
+
+main() {
+    pushd ../..
+    print_env
+    source_env
+    start_otelcol
+    start_cedana
+    start_correctness
+    stop_cedana
+    popd
+}
+
+main
