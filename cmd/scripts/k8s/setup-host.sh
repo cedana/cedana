ae982a1fd42512b48e8f7e8cdb1035a467510fa9
Brandon Smith <brandon.smith@cedana.ai>
2024-08-23 16:21:11 -0500

    fix: working state

--- cmd/scripts/k8s/setup-host.sh
+++ cmd/scripts/k8s/setup-host.sh
@@ -1,31 +1,39 @@
 
 #!/bin/bash
 
-# Install Cedana
+# Copy Cedana binaries to the host
 cp /usr/local/bin/cedana /host/usr/local/bin/cedana
 cp /usr/local/bin/build-start-daemon.sh /host/build-start-daemon.sh
 
-chroot /host <<"EOT"
-YUM_PACKAGES="wget git gcc make libnet-devel protobuf \
-    protobuf-c protobuf-c-devel protobuf-c-compiler \
-    protobuf-compiler protobuf-devel python3-protobuf \
-    libnl3-devel libcap-devel libseccomp-devel gpgme-devel btrfs-progs-devel buildah criu"
+# Enter chroot environment on the host
+chroot /host /bin/bash <<'EOT'
 
-dpkg --configure -a || echo "Failed to configure dpkg"
+# Define packages for YUM and APT
+YUM_PACKAGES=(
+    wget git gcc make libnet-devel protobuf protobuf-c protobuf-c-devel
+    protobuf-c-compiler protobuf-compiler protobuf-devel python3-protobuf
+    libnl3-devel libcap-devel libseccomp-devel gpgme-devel btrfs-progs-devel
+    buildah criu
+)
 
-YUM_PACKAGES=(wget libnet-devel libnl3-devel libcap-devel libseccomp-devel gpgme-devel btrfs-progs-devel buildah criu protobuf protobuf-c protobuf-c-devel protobuf-c-compiler protobuf-compiler protobuf-devel python3-protobuf)
+APT_PACKAGES=(
+    wget libgpgme11-dev libseccomp-dev libbtrfs-dev git make
+    libnl-3-dev libnet-dev libbsd-dev libcap-dev pkg-config
+    libprotobuf-dev python3-protobuf build-essential libprotobuf-c1 buildah
+)
 
-APT_PACKAGES=(wget libgpgme11-dev libseccomp-dev libbtrfs-dev git make libnl-3-dev libnet-dev libbsd-dev libcap-dev pkg-config libprotobuf-dev pkg-config python3-protobuf build-essential libprotobuf-c1 buildah)
-
+# Function to install APT packages
 install_apt_packages() {
     apt-get update
-    apt-get install -y "${APT_PACKAGES[@]}" || echo "Failed to install and setup packages"
+    apt-get install -y "${APT_PACKAGES[@]}" || echo "Failed to install APT packages"
 }
 
-check_and_install_yum_packages() {
-    yum install -y "${YUM_PACKAGES[@]}" || echo "Failed to install and setup packages"
+# Function to install YUM packages
+install_yum_packages() {
+    yum install -y "${YUM_PACKAGES[@]}" || echo "Failed to install YUM packages"
 }
 
+# Function to install CRIU on Ubuntu 22.04
 install_criu_ubuntu_2204() {
     PACKAGE_URL="https://download.opensuse.org/repositories/devel:/tools:/criu/xUbuntu_22.04/amd64/criu_3.19-4_amd64.deb"
     OUTPUT_FILE="criu_3.19-4_amd64.deb"
@@ -35,6 +43,7 @@ install_criu_ubuntu_2204() {
     rm $OUTPUT_FILE
 }
 
+# Detect OS and install appropriate packages
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     case "$ID" in
@@ -42,10 +51,7 @@ if [ -f /etc/os-release ]; then
             install_apt_packages
             install_criu_ubuntu_2204
             ;;
-        rhel | centos | fedora)
-            install_yum_packages
-            ;;
-        amzn)
+        rhel | centos | fedora | amzn)
             install_yum_packages
             ;;
         *)
@@ -53,28 +59,26 @@ if [ -f /etc/os-release ]; then
             exit 1
             ;;
     esac
-elif [ -f /etc.debian_version ]; then
+elif [ -f /etc/debian_version ]; then
     install_apt_packages
     install_criu_ubuntu_2204
-elif [ -f /etc.redhat-release ]; then
-    check_and_install_yum_packages
+elif [ -f /etc/redhat-release ]; then
+    install_yum_packages
 else
     echo "Unknown distribution"
     exit 1
 fi
 
-
-wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz && rm -rf /usr/local/go
-tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz && rm go1.22.0.linux-amd64.tar.gz
+# Install Go
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+rm go1.22.0.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
 
-# Install Cedana
-cp /usr/local/bin/cedana /host/usr/local/bin/cedana
-cp /usr/local/bin/build-start-daemon.sh /host/build-start-daemon.sh
-
-chroot /host /bin/bash -c '
+# Run the Cedana daemon setup script
 cd /
-
 ./build-start-daemon.sh --systemctl --no-build
+
 EOT
