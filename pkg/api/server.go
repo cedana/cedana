c2c123a9386256f30935d6bede56230c98a8daac
Swarnim Arun <swarnimarun11@gmail.com>
2024-09-06 18:29:07 +0530

    fix: doc comment

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -128,9 +128,9 @@ func NewServer(ctx context.Context, opts *ServeOpts) (*Server, error) {
 	if opts.VSOCKEnabled {
 		listener, err = vsock.Listen(VSOCK_PORT, nil)
 	} else {
-		// NOTE: localhost requires firewall settings to disabled inside kubernetes
-		// this may or may not work without kubernetes based on firewall and network
-		// configuration, should work fine on local system, hence use 0.0.0.0
+		// NOTE: `localhost` server inside kubernetes may or may not work
+		// based on firewall and network configuration, it would only work
+		// on local system, hence for serving use 0.0.0.0
 		Address = fmt.Sprintf("0.0.0.0:%d", opts.GrpcPort)
 		listener, err = net.Listen(PROTOCOL, Address)
 	}
