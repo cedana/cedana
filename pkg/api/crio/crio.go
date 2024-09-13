a1dc47d773de2e632152a4b1f37b311c48e3a816
Brandon Smith <brandon.smith@cedana.ai>
2024-09-12 19:27:38 -0500

    fix: remove test code and stop chmod on symlinks

--- pkg/api/crio/crio.go
+++ pkg/api/crio/crio.go
@@ -190,14 +190,6 @@ func RootfsCheckpoint(ctx context.Context, ctrDir, dest, ctrID string, specgen *
 		return "", fmt.Errorf("failed to apply root file-system diff file %s: %w", ctrDir, err)
 	}
 
-	specificFile := "/var/lib/containers/storage/overlay-containers/rootfs-diff/usr/bin/sudo"
-	var originalPerm os.FileMode
-	if fileInfo, err := os.Lstat(specificFile); err == nil {
-		originalPerm = fileInfo.Mode().Perm()
-	} else {
-		log.Error().Msgf("failed to get file info for %s: %s", specificFile, err)
-	}
-
 	// We have to iterate over changes and change the ownership of files for containers that
 	// may be user namespaced, like sysbox containers, which will have uid and gids of their init
 	// process. This is an issue on restore as those files will have nogroup and will cause errors
@@ -229,23 +221,11 @@ func RootfsCheckpoint(ctx context.Context, ctrDir, dest, ctrID string, specgen *
 				log.Debug().Msgf("failed to change ownership for %s: %s", fullPath, err)
 			}
 			log.Debug().Msgf("\t mode is regular: %s", fullPath)
-		}
 
-		if err := os.Chmod(fullPath, perm); err != nil {
-			log.Error().Msgf("failed to restore permissions for %s: %s", fullPath, err)
+			if err := os.Chmod(fullPath, perm); err != nil {
+				log.Error().Msgf("failed to restore permissions for %s: %s", fullPath, err)
+			}
 		}
-
-		sudoInfo, err := os.Lstat(specificFile)
-		if err != nil {
-			log.Error().Msgf("failed to get file info for %s: %s", fullPath, err)
-			break
-		}
-
-		newSudoPerms := sudoInfo.Mode().Perm()
-		if newSudoPerms != originalPerm {
-			log.Debug().Msgf("%s file perms changed after %s", specificFile, fullPath)
-		}
-
 	}
 
 	if err := os.Remove(diffPath); err != nil {
