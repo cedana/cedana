5a276dd24e2689258eac88151e8e10f575331362
rahulk789 <rahul.u.india@gmail.com>
2024-09-18 17:48:23 +0000

    feat: otel get port

--- pkg/utils/otel.go
+++ pkg/utils/otel.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
@@ -68,8 +69,12 @@ func newPropagator() propagation.TextMapPropagator {
 }
 
 func newTraceProvider(ctx context.Context, version string) (*trace.TracerProvider, error) {
+
+	grpcPort := viper.GetInt("otel_port")
+	address := fmt.Sprintf("127.0.0.1:%d", grpcPort)
 	traceExporter, err := otlptracegrpc.New(
 		ctx,
+		otlptracegrpc.WithEndpoint(address),
 		otlptracegrpc.WithInsecure(),
 	)
 	if err != nil {
