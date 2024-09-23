f7f292ce5426fe1b66ac44cc09e873ff5b429368
Brandon Smith <brandon.smith@cedana.ai>
2024-09-23 18:22:58 -0500

    fix: drop temp log levels

--- main.go
+++ main.go
@@ -7,33 +7,12 @@ import (
 
 	"github.com/cedana/cedana/cmd"
 	"github.com/containers/storage/pkg/reexec"
-	"github.com/rs/zerolog"
 )
 
 // loaded from ldflag definitions
 var Version = "dev"
 
 func main() {
-	logLevel := os.Getenv("CEDANA_LOGGER_LEVEL")
-
-	logLevels := map[string]zerolog.Level{
-		"debug":    zerolog.DebugLevel,
-		"info":     zerolog.InfoLevel,
-		"warn":     zerolog.WarnLevel,
-		"error":    zerolog.ErrorLevel,
-		"fatal":    zerolog.FatalLevel,
-		"panic":    zerolog.PanicLevel,
-		"trace":    zerolog.TraceLevel,
-		"disabled": zerolog.Disabled,
-	}
-
-	level, ok := logLevels[logLevel]
-	if !ok {
-		// default logger level
-		level = zerolog.DebugLevel
-	}
-
-	zerolog.SetGlobalLevel(level)
 
 	// Grandparent context to deal with OS interrupts
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
