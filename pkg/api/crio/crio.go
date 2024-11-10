00cbbca1363268cbfbfa379af20249b1dbc9797a
rahulk789 <rahul.u.india+1@gmail.com>
2024-11-10 16:59:37 +0000

    still fixing stuff
    Signed-off-by: rahulk789 <rahul.u.india+1@gmail.com>

--- pkg/api/crio/crio.go
+++ pkg/api/crio/crio.go
@@ -12,14 +12,24 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/cedana/cedana/pkg/api/runc"
+	"github.com/containers/buildah"
+	"github.com/containers/buildah/define"
+	"github.com/containers/buildah/pkg/cli"
+	"github.com/containers/buildah/pkg/parse"
+	"github.com/containers/buildah/util"
 	"github.com/containers/common/pkg/auth"
+	"github.com/containers/image/v5/manifest"
+	"github.com/containers/image/v5/pkg/shortnames"
+	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/containers/image/v5/types"
 	archive "github.com/containers/storage/pkg/archive"
 	libconfig "github.com/cri-o/cri-o/pkg/config"
 	rspec "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/rs/zerolog/log"
+	"github.com/sirupsen/logrus"
 )
 
 func getDefaultConfig() (*libconfig.Config, error) {
@@ -402,6 +412,259 @@ func removeBuildahContainer(containerID string) error {
 		return err
 	}
 
+	return nil
+}
+
+type commitInputOptions struct {
+	authfile           string
+	omitHistory        bool
+	blobCache          string
+	certDir            string
+	changes            []string
+	configFile         string
+	creds              string
+	cwOptions          string
+	disableCompression bool
+	format             string
+	iidfile            string
+	manifest           string
+	omitTimestamp      bool
+	timestamp          int64
+	quiet              bool
+	referenceTime      string
+	rm                 bool
+	pull               string
+	pullAlways         bool
+	pullNever          bool
+	sbomImgOutput      string
+	sbomImgPurlOutput  string
+	sbomMergeStrategy  string
+	sbomOutput         string
+	sbomPreset         string
+	sbomPurlOutput     string
+	sbomScannerCommand []string
+	sbomScannerImage   string
+	signaturePolicy    string
+	signBy             string
+	squash             bool
+	tlsVerify          bool
+	identityLabel      bool
+	encryptionKeys     []string
+	encryptLayers      []int
+	unsetenvs          []string
+	addFile            []string
+}
+
+// Tail returns a string slice after the first element unless there are
+// not enough elements, then it returns an empty slice.  This is to replace
+// the urfavecli Tail method for args
+func Tail(a []string) []string {
+	if len(a) >= 2 {
+		return a[1:]
+	}
+	return []string{}
+}
+func buildahCommit(containerID, newImageRef) {
+	commit(args)
+}
+func commit(args []string, iopts commitInputOptions) error {
+	var dest types.ImageReference
+	if len(args) == 0 {
+		return errors.New("container ID must be specified")
+	}
+	if err := auth.CheckAuthFile(iopts.authfile); err != nil {
+		return err
+	}
+
+	name := args[0]
+	args = Tail(args)
+	if len(args) > 1 {
+		return errors.New("too many arguments specified")
+	}
+	image := ""
+	if len(args) > 0 {
+		image = args[0]
+	}
+	compress := define.Gzip
+	if iopts.disableCompression {
+		compress = define.Uncompressed
+	}
+
+	format, err := cli.GetFormat(iopts.format)
+	if err != nil {
+		return err
+	}
+	store, err := getStore(c)
+	if err != nil {
+		return err
+	}
+
+	ctx := getContext()
+
+	builder, err := buildah.openBuilder(ctx, store, name)
+	if err != nil {
+		return fmt.Errorf("reading build container %q: %w", name, err)
+	}
+
+	systemContext, err := parse.SystemContextFromOptions(c)
+	if err != nil {
+		return fmt.Errorf("building system context: %w", err)
+	}
+
+	// If the user specified an image, we may need to massage it a bit if
+	// no transport is specified.
+	if image != "" {
+		if dest, err = alltransports.ParseImageName(image); err != nil {
+			candidates, err2 := shortnames.ResolveLocally(systemContext, image)
+			if err2 != nil {
+				return err2
+			}
+			if len(candidates) == 0 {
+				return fmt.Errorf("parsing target image name %q", image)
+			}
+			dest2, err2 := storageTransport.Transport.ParseStoreReference(store, candidates[0].String())
+			if err2 != nil {
+				return fmt.Errorf("parsing target image name %q: %w", image, err)
+			}
+			dest = dest2
+		}
+	}
+
+	// Add builder identity information.
+	if iopts.identityLabel {
+		builder.SetLabel(buildah.BuilderIdentityAnnotation, define.Version)
+	}
+
+	encConfig, encLayers, err := cli.EncryptConfig(iopts.encryptionKeys, iopts.encryptLayers)
+	if err != nil {
+		return fmt.Errorf("unable to obtain encryption config: %w", err)
+	}
+
+	var overrideConfig *manifest.Schema2Config
+	if c.Flag("config").Changed {
+		configBytes, err := os.ReadFile(iopts.configFile)
+		if err != nil {
+			return fmt.Errorf("reading configuration blob from file: %w", err)
+		}
+		overrideConfig = &manifest.Schema2Config{}
+		if err := json.Unmarshal(configBytes, &overrideConfig); err != nil {
+			return fmt.Errorf("parsing configuration blob from %q: %w", iopts.configFile, err)
+		}
+	}
+
+	var addFiles map[string]string
+	if len(iopts.addFile) > 0 {
+		addFiles = make(map[string]string)
+		for _, spec := range iopts.addFile {
+			specSlice := strings.SplitN(spec, ":", 2)
+			if len(specSlice) == 1 {
+				specSlice = []string{specSlice[0], specSlice[0]}
+			}
+			if len(specSlice) != 2 {
+				return fmt.Errorf("parsing add-file argument %q: expected 1 or 2 parts, got %d", spec, len(strings.SplitN(spec, ":", 2)))
+			}
+			st, err := os.Stat(specSlice[0])
+			if err != nil {
+				return fmt.Errorf("parsing add-file argument %q: source %q: %w", spec, specSlice[0], err)
+			}
+			if st.IsDir() {
+				return fmt.Errorf("parsing add-file argument %q: source %q is not a regular file", spec, specSlice[0])
+			}
+			addFiles[specSlice[1]] = specSlice[0]
+		}
+	}
+
+	options := buildah.CommitOptions{
+		PreferredManifestType: format,
+		Manifest:              iopts.manifest,
+		Compression:           compress,
+		SignaturePolicyPath:   iopts.signaturePolicy,
+		SystemContext:         systemContext,
+		IIDFile:               iopts.iidfile,
+		Squash:                iopts.squash,
+		BlobDirectory:         iopts.blobCache,
+		OmitHistory:           iopts.omitHistory,
+		SignBy:                iopts.signBy,
+		OciEncryptConfig:      encConfig,
+		OciEncryptLayers:      encLayers,
+		UnsetEnvs:             iopts.unsetenvs,
+		OverrideChanges:       iopts.changes,
+		OverrideConfig:        overrideConfig,
+		ExtraImageContent:     addFiles,
+	}
+	exclusiveFlags := 0
+	if c.Flag("reference-time").Changed {
+		exclusiveFlags++
+		referenceFile := iopts.referenceTime
+		finfo, err := os.Stat(referenceFile)
+		if err != nil {
+			return fmt.Errorf("reading timestamp of file %q: %w", referenceFile, err)
+		}
+		timestamp := finfo.ModTime().UTC()
+		options.HistoryTimestamp = &timestamp
+	}
+	if c.Flag("timestamp").Changed {
+		exclusiveFlags++
+		timestamp := time.Unix(iopts.timestamp, 0).UTC()
+		options.HistoryTimestamp = &timestamp
+	}
+	if iopts.omitTimestamp {
+		exclusiveFlags++
+		timestamp := time.Unix(0, 0).UTC()
+		options.HistoryTimestamp = &timestamp
+	}
+
+	if iopts.cwOptions != "" {
+		confidentialWorkloadOptions, err := parse.GetConfidentialWorkloadOptions(iopts.cwOptions)
+		if err != nil {
+			return fmt.Errorf("parsing --cw arguments: %w", err)
+		}
+		options.ConfidentialWorkloadOptions = confidentialWorkloadOptions
+	}
+
+	pullPolicy, err := parse.PullPolicyFromOptions(c)
+	if err != nil {
+		return err
+	}
+
+	if c.Flag("sbom").Changed || c.Flag("sbom-scanner-command").Changed || c.Flag("sbom-scanner-image").Changed || c.Flag("sbom-image-output").Changed || c.Flag("sbom-merge-strategy").Changed || c.Flag("sbom-output").Changed || c.Flag("sbom-image-output").Changed || c.Flag("sbom-purl-output").Changed || c.Flag("sbom-image-purl-output").Changed {
+		var sbomOptions []define.SBOMScanOptions
+		sbomOption, err := parse.SBOMScanOptions(c)
+		if err != nil {
+			return err
+		}
+		sbomOption.PullPolicy = pullPolicy
+		sbomOptions = append(sbomOptions, *sbomOption)
+		options.SBOMScanOptions = sbomOptions
+	}
+
+	if exclusiveFlags > 1 {
+		return errors.New("can not use more then one timestamp option at at time")
+	}
+
+	if !iopts.quiet {
+		options.ReportWriter = os.Stderr
+	}
+	id, ref, _, err := builder.Commit(ctx, dest, options)
+	if err != nil {
+		return util.GetFailureCause(err, fmt.Errorf("committing container %q to %q: %w", builder.Container, image, err))
+	}
+	if ref != nil && id != "" {
+		logrus.Debugf("wrote image %s with ID %s", ref, id)
+	} else if ref != nil {
+		logrus.Debugf("wrote image %s", ref)
+	} else if id != "" {
+		logrus.Debugf("wrote image with ID %s", id)
+	} else {
+		logrus.Debugf("wrote image")
+	}
+	if options.IIDFile == "" && id != "" {
+		fmt.Printf("%s\n", id)
+	}
+
+	if iopts.rm {
+		return builder.Delete()
+	}
 	return nil
 }
 
