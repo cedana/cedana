305902e8ada0a3740294e3eacfc9016abd66519e
Liana Koleva <43767763+lianakoleva@users.noreply.github.com>
2024-11-20 04:03:56 +0000

    include path in message

--- cmd/dump.go
+++ cmd/dump.go
@@ -63,9 +63,8 @@ var dumpProcessCmd = &cobra.Command{
 		log.Info().Msgf("cmd/dump stream = %d", stream)
 		if stream > 0 {
 			if _, err := exec.LookPath("cedana-image-streamer"); err != nil {
-				log.Error().Msgf("Cannot find cedana-image-streamer in PATH")
 				path := os.Getenv("PATH")
-				log.Error().Msgf("PATH = %v", path)
+				log.Error().Msgf("Cannot find cedana-image-streamer in PATH = %v", path)
 				return err
 			}
 		}
