a2a744d3a68cf7605125c07e1e2b37e1ef6bfa44
Liana Koleva <43767763+lianakoleva@users.noreply.github.com>
2024-12-10 19:12:44 +0000

    clean up

--- pkg/api/dump.go
+++ pkg/api/dump.go
@@ -338,10 +338,6 @@ func (s *service) setupStreamerCapture(ctx context.Context, dumpdir string, gpu 
 	}
 	args = append(args, "capture") // subcommand must be after options
 	cmd := exec.CommandContext(ctx, "cedana-image-streamer", args...)
-	cmd.Env = os.Environ()
-	log.Info().Msgf("cmd env var = %v", cmd.Env)
-	log.Info().Msgf("executing cmd %v", cmd)
-	var err error
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return nil, err
