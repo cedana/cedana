302e5b649656560ed88fa054b117177eb73bbded
Yash Anand <yashanand1910@gmail.com>
2025-01-15 18:24:40 -0500

    Fix setup-host script

--- scripts/k8s-install-plugins.sh
+++ scripts/k8s-install-plugins.sh
@@ -39,7 +39,7 @@ fi
 
 # Install all plugins
 if [[ -n $PLUGINS ]]; then
-    "$APP_PATH" plugin install "$PLUGINS"
+    "$APP_PATH" plugin install $PLUGINS
 fi
 
 # install the shim to containerd, as it was downlaoded by the k8s plugin
