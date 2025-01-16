1498727d7bc961118e5071f45f512898dd9bcca6
Swarnim Arun <swarnimarun11@gmail.com>
2025-01-17 01:48:43 +0530

    hotfix: upgrade cedana k8s broken
    cedana upgrade fails to copy cedana file is running, keep this in case until we can provide a separate upgrade utility and binary

--- plugins/k8s/cmd/scripts/setup-host.sh
+++ plugins/k8s/cmd/scripts/setup-host.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+chroot /host /bin/bash <<'EOT'
+APP_NAME="cedana"
+SERVICE_FILE="/etc/systemd/system/$APP_NAME.service"
+
+if [ -f /var/log/cedana-daemon.log ]; then
+    echo "Stopping $APP_NAME service..."
+    $SUDO_USE systemctl stop $APP_NAME.service
+
+    # truncate the logs
+    echo -n > /var/log/cedana-daemon.log
+else
+    echo "No cedana-daemon.log file found"
+    echo "Skipping $APP_NAME service stop"
+fi
+EOT
 # NOTE: This script assumes it's executed in the container environment
 
 mkdir -p /host/cedana /host/cedana/bin /host/cedana/scripts /host/cedana/lib
