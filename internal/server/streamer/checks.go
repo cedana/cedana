c65c121f0e5142b277bea31741ea1adf495aa6fc
Yash Anand <yashanand1910@gmail.com>
2025-02-04 01:05:59 -0500

    Add kernel settings check to streamer health check

--- internal/server/streamer/checks.go
+++ internal/server/streamer/checks.go
@@ -3,13 +3,24 @@ package streamer
 // Health checks for streamer
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"os"
 	"os/exec"
+	"strconv"
 
 	"buf.build/gen/go/cedana/cedana/protocolbuffers/go/daemon"
 	"github.com/cedana/cedana/pkg/config"
 	"github.com/cedana/cedana/pkg/plugins"
 	"github.com/cedana/cedana/pkg/types"
+	"github.com/cedana/cedana/pkg/utils"
+)
+
+const (
+	OPTIMAL_SOFT_LIMIT = 0
+	OPTIMAL_HARD_LIMIT = 0
+	OPTIMAL_MAX_SIZE   = 4 * utils.MEBIBYTE
 )
 
 func Checks(m plugins.Manager) types.Checks {
@@ -18,6 +29,7 @@ func Checks(m plugins.Manager) types.Checks {
 		List: []types.Check{
 			CheckVersion(m),
 			CheckCriu(m),
+			CheckKernelSettings(),
 		},
 	}
 }
@@ -67,5 +79,57 @@ func CheckCriu(manager plugins.Manager) types.Check {
 		}
 
 		return []*daemon.HealthCheckComponent{component}
+	}
+}
+
+func CheckKernelSettings() types.Check {
+	return func(ctx context.Context) []*daemon.HealthCheckComponent {
+		softLimitComponent := &daemon.HealthCheckComponent{Name: "pipe pages soft limit"}
+		hardLimitComponent := &daemon.HealthCheckComponent{Name: "pipe pages hard limit"}
+		maxSizeComponent := &daemon.HealthCheckComponent{Name: "pipe max size"}
+
+		softLimit, err := os.ReadFile("/proc/sys/fs/pipe-user-pages-soft")
+		if err != nil {
+			softLimitComponent.Errors = append(softLimitComponent.Errors, "Failed to read /proc/sys/fs/pipe-user-pages-soft")
+		} else {
+			softLimit = bytes.TrimSpace(softLimit)
+			if string(softLimit) == fmt.Sprintf("%d", OPTIMAL_SOFT_LIMIT) {
+				softLimitComponent.Data = "unlimited"
+			} else {
+				softLimitComponent.Warnings = append(softLimitComponent.Warnings, fmt.Sprintf("For optimal performance, `echo %d > /proc/sys/fs/pipe-user-pages-soft`", OPTIMAL_SOFT_LIMIT))
+				softLimitComponent.Data = string(softLimit)
+			}
+		}
+
+		hardLimit, err := os.ReadFile("/proc/sys/fs/pipe-user-pages-hard")
+		if err != nil {
+			hardLimitComponent.Errors = append(hardLimitComponent.Errors, "Failed to read /proc/sys/fs/pipe-user-pages-hard")
+		} else {
+			hardLimit = bytes.TrimSpace(hardLimit)
+			if string(hardLimit) == fmt.Sprintf("%d", OPTIMAL_HARD_LIMIT) {
+				hardLimitComponent.Data = "unlimited"
+			} else {
+				hardLimitComponent.Warnings = append(hardLimitComponent.Warnings, fmt.Sprintf("For optimal performance, `echo %d > /proc/sys/fs/pipe-user-pages-hard`", OPTIMAL_HARD_LIMIT))
+				hardLimitComponent.Data = string(hardLimit)
+			}
+		}
+
+		maxSize, err := os.ReadFile("/proc/sys/fs/pipe-max-size")
+		if err != nil {
+			maxSizeComponent.Errors = append(maxSizeComponent.Errors, "Failed to read /proc/sys/fs/pipe-max-size")
+		} else {
+      maxSize = bytes.TrimSpace(maxSize)
+			maxSizeBytes, err := strconv.ParseInt(string(maxSize), 10, 64)
+			if err != nil {
+				maxSizeComponent.Errors = append(maxSizeComponent.Errors, "Failed to parse /proc/sys/fs/pipe-max-size")
+			} else {
+				maxSizeComponent.Data = utils.SizeStr(maxSizeBytes)
+				if maxSizeBytes < OPTIMAL_MAX_SIZE {
+					maxSizeComponent.Warnings = append(maxSizeComponent.Warnings, fmt.Sprintf("For optimal performance, `echo %d > /proc/sys/fs/pipe-max-size`", OPTIMAL_MAX_SIZE))
+				}
+			}
+		}
+
+		return []*daemon.HealthCheckComponent{softLimitComponent, hardLimitComponent, maxSizeComponent}
 	}
 }
