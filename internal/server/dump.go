6fb68be8b4718194c96da87b06729932753ff858
Brandon Smith <brandon.smith@cedana.ai>
2025-02-26 18:35:19 -0600

    chore: move DumpVM rpc function to separate file

--- internal/server/dump.go
+++ internal/server/dump.go
@@ -12,7 +12,6 @@ import (
 	"github.com/cedana/cedana/internal/server/process"
 	"github.com/cedana/cedana/internal/server/streamer"
 	"github.com/cedana/cedana/internal/server/validation"
-	"github.com/cedana/cedana/internal/server/vm"
 	"github.com/cedana/cedana/pkg/config"
 	"github.com/cedana/cedana/pkg/features"
 	"github.com/cedana/cedana/pkg/types"
@@ -21,39 +20,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-func (s *Server) DumpVM(ctx context.Context, req *daemon.DumpVMReq) (*daemon.DumpVMResp, error) {
-
-	middleware := types.Middleware[types.DumpVM]{
-		validation.ValidateDumpVMRequest,
-		filesystem.PrepareDumpVMDir,
-
-		pluginDumpVMMiddleware, // middleware from plugins
-
-	}
-
-	dump := vm.Dump.With(middleware...)
-
-	opts := types.Opts{
-		Lifetime: s.lifetime,
-		Plugins:  s.plugins,
-		WG:       s.wg,
-	}
-	resp := &daemon.DumpVMResp{}
-
-	vm := vm.New[daemon.DumpVMReq, daemon.DumpVMResp](s.plugins)
-
-	_, err := vm(dump)(ctx, opts, resp, req)
-	if err != nil {
-		return nil, err
-	}
-
-	if utils.PathExists(resp.TarDumpDir) {
-		log.Info().Str("path", resp.TarDumpDir).Str("type", req.Type).Msg("dump successful")
-	}
-
-	return resp, nil
-}
 
 func (s *Server) Dump(ctx context.Context, req *daemon.DumpReq) (*daemon.DumpResp, error) {
 	// The order below is the order followed before executing
@@ -113,31 +79,6 @@ func (s *Server) Dump(ctx context.Context, req *daemon.DumpReq) (*daemon.DumpRes
 //////////////////////////
 //// Helper Adapters /////
 //////////////////////////
-
-// Adapter that inserts new adapters after itself based on the type of dump.
-func pluginDumpVMMiddleware(next types.DumpVM) types.DumpVM {
-	return func(ctx context.Context, opts types.Opts, resp *daemon.DumpVMResp, req *daemon.DumpVMReq) (exited chan int, err error) {
-		middleware := types.Middleware[types.DumpVM]{}
-		t := req.GetType()
-		switch t {
-		case "cloud-hypervisor":
-			// nothing to do, we only support clh
-		default:
-			// Insert plugin-specific middleware
-			err = features.DumpVMMiddleware.IfAvailable(func(
-				name string,
-				pluginMiddleware types.Middleware[types.DumpVM],
-			) error {
-				middleware = append(middleware, pluginMiddleware...)
-				return nil
-			}, t)
-			if err != nil {
-				return nil, status.Error(codes.Unimplemented, err.Error())
-			}
-		}
-		return next.With(middleware...)(ctx, opts, resp, req)
-	}
-}
 
 // Adapter that inserts new adapters after itself based on the type of dump.
 func pluginDumpMiddleware(next types.Dump) types.Dump {
