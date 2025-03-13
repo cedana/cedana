752d5a299df569c0abf1ccdd91599319c0200ee3
Swarnim Arun <swarnimarun11@gmail.com>
2025-03-14 01:02:47 +0530

    fix: try fix stuck ci

--- test/regression/helpers/containerd.bash
+++ test/regression/helpers/containerd.bash
@@ -1,6 +1,6 @@
 
 setup_containerd() {
-    echo "$(ctr --help)"
+    # echo "$(ctr --help)"
     echo "ensure containerd service is enabled"
     run containerd &
     echo "$(ps aux | grep containerd)"
