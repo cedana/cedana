46e2e55e555b7f492ce562abed300ffc59a792ac
Niranjan Ravichandra <nravic@cedana.ai>
2025-05-23 14:20:51 -0400

    feat:version

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,6 +2,10 @@ package version
 
 var Version = "dev"
 
+func PutVersion(version string) {
+	Version = version
+}
+
 func GetVersion() string {
 	return Version
 }
--- main.go
+++ main.go
@@ -10,13 +10,15 @@ import (
 	"github.com/cedana/cedana/internal/version"
 )
 
+// loaded from ldflag definitions
+var Version = "dev"
+
 func main() {
 	// Grandparent context to deal with OS interrupts
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
 	defer stop()
-
-	Version := version.GetVersion()
+	version.PutVersion(Version)
 
 	if err := cmd.Execute(ctx, Version); err != nil {
 		os.Exit(1)
