c23ed626650f54693c6df9978dd5add442dbbe11
Yash Anand <yashanand1910@gmail.com>
2025-06-04 00:59:09 -0400

    Fix console restore

--- internal/cedana/gpu/controller.go
+++ internal/cedana/gpu/controller.go
@@ -261,7 +261,7 @@ func (p *pool) Terminate(id string) {
 	if c.PID == 0 {
 		return
 	}
-	syscall.Kill(-int(c.PID), CONTROLLER_TERMINATE_SIGNAL) // To process group so even its workers are killed
+	syscall.Kill(int(c.PID), CONTROLLER_TERMINATE_SIGNAL) // To process group so even its workers are killed
 	if c.ClientConn != nil {
 		c.ClientConn.Close()
 		c.ClientConn = nil
@@ -269,7 +269,7 @@ func (p *pool) Terminate(id string) {
 	}
 	if !c.External {
 		// If we are the parent process of the GPU controller, we should clean it up properly
-		syscall.Wait4(int(c.PID), nil, syscall.WNOHANG, nil)
+		syscall.Wait4(int(c.PID), nil, 0, nil)
 	}
 }
 
