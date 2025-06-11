81f78cbb1e37b41804995a975927d31f862af79b
Yash Anand <yashanand1910@gmail.com>
2025-06-11 18:23:40 -0400

    Fix stdio inheritance during restore

--- internal/cedana/process/restore_adapters.go
+++ internal/cedana/process/restore_adapters.go
@@ -166,14 +166,28 @@ func InheritFilesForRestore(next types.Restore) types.Restore {
 						return nil, status.Errorf(codes.FailedPrecondition,
 							"found open file %s with fd %d which is a TTY and so restoring will fail because no TTY to inherit. Try --no-server restore", f.Path, f.Fd)
 					}
-					extraFiles = append(extraFiles, os.Stdin)
-
-					inheritFds = append(inheritFds, &criu_proto.InheritFd{
-						Fd:  proto.Int32(int32(2 + len(extraFiles))),
-						Key: proto.String(path),
-					})
+					if f.Fd == 0 {
+						extraFiles = append(extraFiles, os.Stdin)
+						inheritFds = append(inheritFds, &criu_proto.InheritFd{
+							Fd:  proto.Int32(int32(2 + len(extraFiles))),
+							Key: proto.String(path),
+						})
+					} else if f.Fd == 1 {
+						extraFiles = append(extraFiles, os.Stdout)
+						inheritFds = append(inheritFds, &criu_proto.InheritFd{
+							Fd:  proto.Int32(int32(2 + len(extraFiles))),
+							Key: proto.String(path),
+						})
+					} else if f.Fd == 2 {
+						extraFiles = append(extraFiles, os.Stderr)
+						inheritFds = append(inheritFds, &criu_proto.InheritFd{
+							Fd:  proto.Int32(int32(2 + len(extraFiles))),
+							Key: proto.String(path),
+						})
+					}
 				} else if f.Fd == 0 {
 					if req.Attachable {
+						log.Debug().Msgf("found open STDIN file %s with fd %d and req.Attachable is set so inheriting it", path, f.Fd)
 						inheritFds = append(inheritFds, &criu_proto.InheritFd{
 							Fd:  proto.Int32(int32(f.Fd)),
 							Key: proto.String(path),
@@ -182,6 +196,7 @@ func InheritFilesForRestore(next types.Restore) types.Restore {
 						log.Warn().Msgf("found open non-TTY STDIN file %s with fd %d and req.Attachable is not set so assuming it still exists", path, f.Fd)
 					}
 				} else if f.Fd == 1 || f.Fd == 2 {
+					log.Debug().Msgf("found open STDOUT/STDERR file %s with fd %d and req.Log/Attachable is set so inheriting it", path, f.Fd)
 					if req.Attachable || req.Log != "" {
 						inheritFds = append(inheritFds, &criu_proto.InheritFd{
 							Fd:  proto.Int32(int32(f.Fd)),
