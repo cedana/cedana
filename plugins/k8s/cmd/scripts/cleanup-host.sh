c1b37b0d3f26e79230612159f269158dd677e049
Yash Anand <yashanand1910@gmail.com>
2025-06-26 16:17:04 -0400

    Revert Destroy daemon on SIGTERM in k8s

--- plugins/k8s/cmd/scripts/cleanup-host.sh
+++ plugins/k8s/cmd/scripts/cleanup-host.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-chroot /host make -C /cedana reset
-chroot /host rm -rf /cedana
-echo "Environment reset completed. Cedana has been uninstalled."
+chroot /host sh -c '
+if [ -d /cedana ]; then
+    make -C /cedana reset
+    rm -rf /cedana
+    echo "Environment reset completed. Cedana has been uninstalled."
+else
+    echo "/cedana does not exist. Nothing to reset."
+fi
+'
