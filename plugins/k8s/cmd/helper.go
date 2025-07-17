f6336d3634e1730faa52acb44613abd498c92039
rahulk789 <rahul.u.india+1@gmail.com>
2025-07-17 23:06:30 +0530

    fix: remove concurrancy totally
    Signed-off-by: rahulk789 <rahul.u.india+1@gmail.com>

--- plugins/k8s/cmd/helper.go
+++ plugins/k8s/cmd/helper.go
@@ -450,7 +450,6 @@ func (es *EventStream) ConsumeCheckpointRequest(address, protocol string) (*rabb
 		es.conn,
 		queueName,
 		rabbitmq.WithConsumerOptionsExchangeName("daemon_broadcast_request"),
-		rabbitmq.WithConsumerOptionsConcurrency(10),
 		rabbitmq.WithConsumerOptionsExchangeDeclare,
 		rabbitmq.WithConsumerOptionsExchangeKind("fanout"),
 		rabbitmq.WithConsumerOptionsConsumerName("cedana_helper"),
