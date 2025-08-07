6f4c4aacf5d7b6cbe37de5a91e297d3c65f9549c
James Gornet <gornetj@gmail.com>
2025-08-07 15:02:46 -0700

    Added nvidia-smi check as a failover condition

--- scripts/host/k8s-install-plugins.sh
+++ scripts/host/k8s-install-plugins.sh
@@ -30,7 +30,7 @@ if [ -d /proc/driver/nvidia/gpus/ ]; then
     if [ ! -d /run/driver/nvidia ]; then
         # Check if the NVIDIA driver is installed by checking the version
         # as nvidia-smi is not installed by GPU Operator
-        if [ -r /proc/driver/nvidia/version ]; then
+        if [ -r /proc/driver/nvidia/version ] || command -v nvidia-smi >/dev/null 2>&1; then
             PLUGINS="$PLUGINS gpu@$CEDANA_PLUGINS_GPU_VERSION"
             echo "Detected NVIDIA GPU! Ensuring CUDA drivers are installed..."
             echo "Driver version is $(nvidia-smi --query-gpu=driver_version --format=csv,noheader)"
