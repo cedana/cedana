a6e1f09b85cc321c0bf3b6a85b7680843f91d479
rahulk789 <rahul.u.india@gmail.com>
2025-12-03 01:04:53 +0530

    fix: check for existence before creation

--- test/helpers/nebius.bash
+++ test/helpers/nebius.bash
@@ -34,6 +34,14 @@ configure_nebius_credentials() {
     printf '%s' "$NB_SA_PRIVATE_KEY" > "$NB_SA_PRIVATE_KEY_PATH"
     chmod 600 "$NB_SA_PRIVATE_KEY_PATH"
 
+    export NB_CLUSTER_ID=$(
+        nebius mk8s cluster get-by-name \
+            --name "$H100_CLUSTER_NAME" \
+            --format json | jq -r '.metadata.id'
+    )
+    if [ -n "$CLUSTER_ID" ] && [ "$CLUSTER_ID" != "null" ]; then
+        debug_log "Cluster already exists, skipp creation..."
+    else
     nebius profile create \
         --endpoint api.nebius.cloud \
         --service-account-id "$NB_SA_ID" \
@@ -41,6 +49,7 @@ configure_nebius_credentials() {
         --private-key-file "$NB_SA_PRIVATE_KEY_PATH" \
         --profile "$NB_SA_PROFILE_NAME" \
         --parent-id "$NB_PROJECT_ID"
+    fi
 
     debug_log "Nebius credentials configured"
 }
@@ -64,17 +73,24 @@ create_nebius_nodegroup() {
     debug_log "Creating Nebius node-group with H100..."
 
     export NB_NODEGROUP_NAME="github-ci-H100"
-    nebius mk8s node-group create \
-        --name "$NB_NODEGROUP_NAME" \
-        --parent-id "$NB_CLUSTER_ID" \
-        --template-boot-disk-size-bytes 137438953472 \
-        --fixed-node-count 1 \
-        --template-resources-platform "gpu-h100-sxm" \
-        --template-resources-preset "1gpu-16vcpu-200gb" \
-        --template-network-interfaces "[{\"public_ip_address\": {},
-                                   \"subnet_id\": \"$NB_SUBNET_ID\"}]"
+    EXISTING_NODEGROUP=$(nebius mk8s node-group list --parent-id "$NB_CLUSTER_ID" \
+        --format json | jq -r ".items[] | select(.metadata.name==\"$NB_NODEGROUP_NAME\") | .metadata.id")
+    if [ -n "$EXISTING_NODEGROUP" ] && [ "$EXISTING_NODEGROUP" != "null" ]; then
+        debug_log "Node-group already exists, skipping creation..."
+        export NB_NODEGROUP_ID="$EXISTING_NODEGROUP"
+    else
+        debug_log "Creating new node-group..."
+        nebius mk8s node-group create \
+            --name "$NB_NODEGROUP_NAME" \
+            --parent-id "$NB_CLUSTER_ID" \
+            --template-boot-disk-size-bytes 137438953472 \
+            --fixed-node-count 1 \
+            --template-resources-platform "gpu-h100-sxm" \
+            --template-resources-preset "1gpu-16vcpu-200gb" \
+            --template-network-interfaces "[{\"public_ip_address\": {},\"subnet_id\": \"$NB_SUBNET_ID\"}]"
 
-    debug_log "Nebius node-group with H100 has been created"
+        debug_log "Nebius node-group with H100 has been created"
+    fi
 }
 
 setup_nebius_cluster() {
