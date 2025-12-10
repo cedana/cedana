fc889a1889681fd5b053b309360f59fbab97cc5b
Niranjan Ravichandra <nravic@cedana.ai>
2025-12-10 12:39:44 -0500

    fix: skip fds

--- internal/cedana/process/restore_adapters.go
+++ internal/cedana/process/restore_adapters.go
@@ -157,8 +157,12 @@ func InheritFilesForRestore(next types.Restore) types.Restore {
 					key = fmt.Sprintf("tty[%x:%x]", f.Rdev, f.Dev)
 					fd = int32(f.Fd)
 				} else {
-					key = fmt.Sprintf("file[%x:%x]", f.MountID, f.Inode)
-					fd = int32(f.Fd)
+					if f.MountID == 0 && f.Inode == 0 {
+						log.Warn().Msgf("skipping open file %s with fd %d which has invalid mountID/inode", f.Path, f.Fd)
+					} else {
+						key = fmt.Sprintf("file[%x:%x]", f.MountID, f.Inode)
+						fd = int32(f.Fd)
+					}
 				}
 				log.Warn().Msgf("inherited external file %s with fd %d. assuming it still exists", f.Path, f.Fd)
 
