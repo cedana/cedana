804007fb8174863bd49e620c0a731b68b0c27df1
Niranjan Ravichandra <nravic@cedana.ai>
2025-12-10 13:01:51 -0500

    fix: cleanup

--- internal/cedana/process/restore_adapters.go
+++ internal/cedana/process/restore_adapters.go
@@ -162,10 +162,9 @@ func InheritFilesForRestore(next types.Restore) types.Restore {
 					} else {
 						key = fmt.Sprintf("file[%x:%x]", f.MountID, f.Inode)
 						fd = int32(f.Fd)
+						log.Warn().Msgf("inherited external file %s with fd %d. assuming it still exists", f.Path, f.Fd)
 					}
 				}
-				log.Warn().Msgf("inherited external file %s with fd %d. assuming it still exists", f.Path, f.Fd)
-
 			} else {
 
 				// Inherit stdio files that are not external
