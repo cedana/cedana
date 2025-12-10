eb16024c4b5743d74dd3aff370297cd9a2baa9ce
Brandon <brandon.smith@cedana.ai>
2025-12-10 13:45:51 -0600

    chore: change pod ip naming to runc level naming

--- plugins/containerd/pkg/keys/dump.go
+++ plugins/containerd/pkg/keys/dump.go
@@ -5,5 +5,5 @@ const (
 	DUMP_RUNTIME_KEY              = "containerd.runtime"
 	DUMP_RW_LAYER_MANIFEST_KEY    = "rw-layer.manifest"
 	DUMP_RW_LAYER_BATCH_FORMATTER = "rw-layer-%d.img"
-	DUMP_PODIP_KEY                = "containerd.pod_ip"
+	DUMP_NETNS_ETH0_IPV4ADDR_KEY  = "containerd.netns_eth0_ipv4addr"
 )
