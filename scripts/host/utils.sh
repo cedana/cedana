8f65bce1b6c9468e950d487034745ae1e948cf2c
Yash Anand <yashanand1910@gmail.com>
2025-12-18 10:37:05 -0500

    Fix script tests

--- scripts/host/utils.sh
+++ scripts/host/utils.sh
@@ -13,7 +13,7 @@ export SERVICE_FILE="/etc/systemd/system/$APP_NAME.service"
 USER=$(whoami)
 export USER
 
-export ENV=production
+export ENV=${ENV:-"production"}
 if pgrep -f "k3s server" >/dev/null 2>&1; then
     ENV="k3s"
 elif [ -f /.dockerenv ]; then
