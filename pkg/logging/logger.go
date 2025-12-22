44c6e249e0a54814447e080caed2bbb853d12aed
Yash Anand <yashanand1910@gmail.com>
2025-12-21 21:18:04 -0500

    CED-1802: Remove forceCleanup from Helm chart in tests & fix logging/metrics

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -36,10 +36,10 @@ func (h LineInfoHook) Run(e *zerolog.Event, l zerolog.Level, msg string) {
 }
 
 func init() {
-	InitLogger(config.Global.LogLevel, io.Discard)
+	initLogger(config.Global.LogLevel, io.Discard)
 }
 
-func InitLogger(level string, writers ...io.Writer) {
+func initLogger(level string, writers ...io.Writer) {
 	var err error
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 
