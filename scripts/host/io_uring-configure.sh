724f8e470363a353e7158801fc5257d4bae45aa0
rahulk789 <rahul.u.india@gmail.com>
2026-01-21 22:51:59 +0530

    fix: propagator env, iouring true for now

--- scripts/host/io_uring-configure.sh
+++ scripts/host/io_uring-configure.sh
@@ -3,6 +3,8 @@ set -e
 
 # XXX: Hack - disable io_uring
 # disable only if sysctl exists
-if sysctl -a 2>/dev/null | grep -q kernel.io_uring_disabled; then
-    sudo sysctl -w kernel.io_uring_disabled=2
+if [ "$IO_URING" = "false" ]; then
+    if sysctl -a 2>/dev/null | grep -q kernel.io_uring_disabled; then
+        sudo sysctl -w kernel.io_uring_disabled=2
+    fi
 fi
