9ebff62e691ebe38cb59cd960f64ae410e1b9948
rahulk789 <rahul.u.india@gmail.com>
2026-01-22 21:12:16 +0530

    fix: improve logging

--- scripts/host/io_uring-configure.sh
+++ scripts/host/io_uring-configure.sh
@@ -4,7 +4,11 @@ set -e
 # XXX: Hack - disable io_uring
 # disable only if sysctl exists
 if [ "$DISABLE_IO_URING" = "true" ]; then
+    echo "Disabling IO Uring using sysctl"
     if sysctl -a 2>/dev/null | grep -q kernel.io_uring_disabled; then
         sudo sysctl -w kernel.io_uring_disabled=2
+        echo "IO Uring disabled successfully"
+    else
+        echo "Failed to disable IO Uring"
     fi
 fi
