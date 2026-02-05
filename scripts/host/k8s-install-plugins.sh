c9e0d3fb86747ff5b4fe7fe6cccc5266d3114d56
Yash Anand <yashanand1910@gmail.com>
2026-02-05 18:17:22 -0500

    Update script

--- scripts/host/k8s-install-plugins.sh
+++ scripts/host/k8s-install-plugins.sh
@@ -103,7 +103,7 @@ if [[ "$CEDANA_PLUGINS_BUILDS" != "local" && "$PLUGINS" != "" ]]; then
 
     if [[ "$PLUGINS_TO_REMOVE" != "" ]]; then
         # shellcheck disable=SC2086
-        "$APP_PATH" plugin remove $PLUGINS_TO_REMOVE || true
+        "$APP_PATH" plugin remove $PLUGINS_TO_REMOVE 2>/dev/null || true
     fi
 fi
 
