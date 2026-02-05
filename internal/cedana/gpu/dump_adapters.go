c778d6c9c84c04edd65614f90f9c6703c2394992
Yash Anand <yashanand1910@gmail.com>
2026-02-04 20:11:11 -0500

    CED-1875: Fix C/R GPU containers when PreStartHook is present

--- internal/cedana/gpu/dump_adapters.go
+++ internal/cedana/gpu/dump_adapters.go
@@ -2,11 +2,32 @@ package gpu
 
 import (
 	"context"
+	"fmt"
+	"regexp"
 
 	"buf.build/gen/go/cedana/cedana/protocolbuffers/go/daemon"
 	"github.com/cedana/cedana/pkg/types"
+	"github.com/cedana/cedana/pkg/utils"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+)
+
+// NOTE: Add any other known NVIDIA GPU mount paths here.
+var NVIDIA_MOUNTS_PATTERN = regexp.MustCompile(
+	`^(` +
+		`/driver/nvidia/gpus|` +
+		`/dev/nvidia\d+|` +
+		`/nvidia|` +
+		`/usr/bin/nvidia|` +
+		`/usr/lib/firmware/nvidia|` +
+		`/usr/lib/libcuda|` +
+		`/usr/lib64/libcuda|` +
+		`/usr/lib/libnvidia|` +
+		`/usr/lib64/libnvidia|` +
+		`/usr/lib/x86_64-linux-gnu/libnvidia|` +
+		`/usr/lib64/x86_64-linux-gnu/libnvidia` +
+		`)`,
 )
 
 // Adapter that adds GPU dump to the request.
@@ -41,7 +62,32 @@ func Dump(gpus Manager) types.Adapter[types.Dump] {
 			// Import GPU CRIU callbacks
 			opts.CRIUCallback.Include(gpus.CRIUCallback(id))
 
+			next = next.With(AddMountsForDump)
+
 			return next(ctx, opts, resp, req)
 		}
+	}
+}
+
+// Adapter that tells CRIU about the external GPU mounts.
+func AddMountsForDump(next types.Dump) types.Dump {
+	return func(ctx context.Context, opts types.Opts, resp *daemon.DumpResp, req *daemon.DumpReq) (code func() <-chan int, err error) {
+		state := resp.GetState()
+		if state == nil {
+			return nil, status.Errorf(
+				codes.InvalidArgument,
+				"missing state. at least PID is required in resp.state",
+			)
+		}
+
+		utils.WalkTree(state, "Mounts", "Children", func(m *daemon.Mount) bool {
+			if NVIDIA_MOUNTS_PATTERN.MatchString(m.Root) {
+				log.Debug().Str("root", m.Root).Str("mount_path", m.MountPoint).Msg("marking NVIDIA GPU mount as external")
+				req.Criu.External = append(req.Criu.External, fmt.Sprintf("mnt[%s]:%s", m.MountPoint, m.MountPoint))
+			}
+			return true
+		})
+
+		return next(ctx, opts, resp, req)
 	}
 }
