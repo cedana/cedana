5e00b23d59c0da4f28f80ef52bcdfa048c8d10cd
Yash Anand <yashanand1910@gmail.com>
2026-02-27 03:38:52 -0500

    Add script sourcing

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -18,6 +18,15 @@ elif [ -f /.dockerenv ]; then
 fi
 export ENV
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+log_info() { echo -e "${GREEN}[INFO]${NC} $1"; }
+log_warn() { echo -e "${YELLOW}[WARN]${NC} $1"; }
+log_error() { echo -e "${RED}[ERROR]${NC} $1" >&2; }
+
 check_root() {
     if [[ "$EUID" -ne 0 ]]; then
         echo "This script must be run as root" >&2
