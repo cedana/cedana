38de409b3224774fb8c86928fba4183d23d2e88e
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2026-04-19 16:08:03 +0530

    try: set rstsibling to false

--- plugins/slurm/internal/defaults/restore_adapters.go
+++ plugins/slurm/internal/defaults/restore_adapters.go
@@ -5,6 +5,7 @@ import (
 
 	"buf.build/gen/go/cedana/cedana/protocolbuffers/go/daemon"
 	"buf.build/gen/go/cedana/cedana/protocolbuffers/go/plugins/slurm"
+	criu_proto "buf.build/gen/go/cedana/criu/protocolbuffers/go/criu"
 	"github.com/cedana/cedana/pkg/types"
 	"google.golang.org/protobuf/proto"
 )
@@ -20,6 +21,11 @@ func FillMissingRestoreDefaults(next types.Restore) types.Restore {
 		if req.GetDetails().GetSlurm().GetID() == "" {
 			req.Details.Slurm.ID = req.GetDetails().GetJID()
 		}
+		if req.GetCriu() == nil {
+			req.Criu = &criu_proto.CriuOpts{}
+		}
+
+		req.Criu.RstSibling = proto.Bool(false)
 		req.Criu.OrphanPtsMaster = proto.Bool(true)
 
 		return next(ctx, opts, resp, req)
