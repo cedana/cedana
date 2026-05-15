76c1d50aee92716c43e06caf89d5a24ad8e77ae1
Brandon <brandon.smith@cedana.ai>
2026-05-15 12:06:43 -0500

    hotfix: merge config

--- plugins/slurm/scripts/install.sh
+++ plugins/slurm/scripts/install.sh
@@ -6,6 +6,6 @@ check_root
 # NOTE: This is called from a Cedana binary so assuming it's already installed
 
 # Re-initialize config since it's a fresh install
-/usr/local/bin/cedana --init-config version
+/usr/local/bin/cedana --merge-config version
 
 echo "Installed Cedana into the host filesystem"
