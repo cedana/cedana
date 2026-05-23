1970f0f7b908d5a254610104b118da5f5dad43ec
Brandon <brandon.smith@cedana.ai>
2026-05-22 20:10:25 -0500

    fix: app path lol

--- plugins/slurm/scripts/install.sh
+++ plugins/slurm/scripts/install.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 # NOTE: This is called from a Cedana binary so assuming it's already installed
 
 # Merge new config defaults without overwriting existing user config values
-/usr/local/bin/cedana --merge-config version
+$APP_PATH --merge-config version
 
 echo "Installed Cedana into the host filesystem"
