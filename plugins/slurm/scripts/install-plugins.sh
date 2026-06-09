49c1741ffdc5c454d4509750dae3fe7709d18866
Yash Anand <yashanand1910@gmail.com>
2026-06-09 19:32:22 -0400

    Fix plugin auto detection

--- plugins/slurm/scripts/install-plugins.sh
+++ plugins/slurm/scripts/install-plugins.sh
@@ -35,6 +35,7 @@ if [[ "$CEDANA_PLUGINS_SLURM_WLM_VERSION" == "latest" ]]; then
     # Example output: "v0.9.291-slurm-25-11-5-1"
     # We extract just the version part: "v0.9.291"
     latest_version=$($APP_PATH plugin list slurm/wlm | awk '/AVAILABLE VERSION/ {getline; print $NF}')
+    [[ -n "$latest_version" ]] || { echo "Error: could not parse version from 'cedana plugin list slurm/wlm'" >&2; exit 1; }
     CEDANA_PLUGINS_SLURM_WLM_VERSION=$(echo "$latest_version" | grep -oP '^v[0-9]+\.[0-9]+\.[0-9]+')
     detected_slurm_version=$(_detect_slurm_version)
     if [ -n "$detected_slurm_version" ]; then
