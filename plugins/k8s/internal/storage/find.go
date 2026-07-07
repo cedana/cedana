baf08a9c2110456378ea8d4478505939d19846f7
Bhavik Sachdev <b.sachdev1904@gmail.com>
2026-07-07 18:14:59 +0530

    remove checks

--- plugins/k8s/internal/storage/find.go
+++ plugins/k8s/internal/storage/find.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
@@ -13,28 +12,12 @@ const (
 )
 
 // Assume POD_ID is available at the POD_ID_ENV_VAR
-// and the host file system is mounted at /host
 func FindDiskEmptyDir() (string, error) {
-	root, err := os.Readlink("/proc/self/root")
-	if err != nil {
-		fmt.Printf("could not open proc root file, err: %v\n", err)
-	} else {
-		log.Printf("root: %s\n", root)
-	}
-
-	mntNs, err := os.Readlink("/proc/self/ns/mnt")
-	if err != nil {
-		fmt.Printf("could not open proc root file, err: %v\n", err)
-	} else {
-		log.Printf("mntNs: %s\n", mntNs)
-	}
-
 	podID := os.Getenv(POD_ID_ENV_VAR)
 	if podID == "" {
 		return "", fmt.Errorf("could not get POD ID")
 	}
 
-	log.Printf("BHAVIK: found pod id: %v\n", podID)
 	path := fmt.Sprintf(DISK_EMPTY_PATH, podID)
 	return path, nil
 }
