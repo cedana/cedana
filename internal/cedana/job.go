b9debeab81d4dae6f6bff23ea562a1f9d779373c
Bhavik Sachdev <b.sachdev1904@gmail.com>
2026-08-24 23:37:54 +0530

    wip

--- internal/cedana/job.go
+++ internal/cedana/job.go
@@ -3,9 +3,11 @@ package cedana
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"buf.build/gen/go/cedana/cedana/protocolbuffers/go/daemon"
 	"github.com/cedana/cedana/internal/cedana/job"
+	"github.com/cedana/cedana/pkg/features"
 	cedana_io "github.com/cedana/cedana/pkg/io"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -127,16 +129,39 @@ func (s *Server) ListCheckpoints(ctx context.Context, req *daemon.ListCheckpoint
 }
 
 func (s *Server) DeleteCheckpoint(ctx context.Context, req *daemon.DeleteCheckpointReq) (*daemon.DeleteCheckpointResp, error) {
-	if req.ID == "" {
+	if req.ID == "" && !req.HasPath() {
 		return nil, status.Errorf(codes.InvalidArgument, "ID must be provided")
 	}
 
-	checkpoint := s.jobs.GetCheckpoint(req.GetID())
-	if checkpoint == nil {
-		return nil, status.Errorf(codes.NotFound, "checkpoint not found")
-	}
+	if req.HasPath() {
+		var storage cedana_io.Storage
+		var err error
+		checkpointPath := req.GetPath()
+		if strings.Contains(checkpointPath, "://") {
+			pluginName := fmt.Sprintf("storage/%s", strings.Split(checkpointPath, "://")[0])
+			err = features.Storage.IfAvailable(func(name string, newPluginStorage func(context.Context) (cedana_io.Storage, error)) error {
+				if newPluginStorage == nil {
+					return fmt.Errorf("plugin '%s' does not implement '%s'", name, features.Storage)
+				}
+				storage, err = newPluginStorage(ctx)
+				return err
+			}, pluginName)
+			if err != nil {
+				return nil, status.Error(codes.Unavailable, err.Error())
+			}
 
-	s.jobs.DeleteCheckpoint(req.GetID())
+      err := storage.Delete(ctx, checkpointPath)
+      if err != nil {
+        return nil, status.Error(codes.Internal, err.Error())
+      }
+		}
+	} else {
+		checkpoint := s.jobs.GetCheckpoint(req.GetID())
+		if checkpoint == nil {
+			return nil, status.Errorf(codes.NotFound, "checkpoint not found")
+		}
+		s.jobs.DeleteCheckpoint(req.GetID())
+	}
 
 	return &daemon.DeleteCheckpointResp{}, nil
 }
